f13a1691d0fb7e47e9f8ce58f4d7730fd5366f6d
fr4nck <f.bellardie@gmail.com>
2026-08-14 18:30:17 +0200

    compat: conserver l'API pyttsx via pyttsx3

---
+++ noethys/pyttsx.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Compatibilité Noethys avec le moteur vocal moderne pyttsx3.
+
+Le code historique peut encore importer ``pyttsx``. Ce module conserve cette
+API minimale sans imposer une modification diffuse des modules applicatifs.
+"""
+from pyttsx3 import *  # noqa: F401,F403
+from pyttsx3 import init
+
+__all__ = ["init"]
