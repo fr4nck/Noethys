d488612c3fb8f26cac3ee19ec133662a0ff0fd3d
fr4nck <f.bellardie@gmail.com>
2026-08-20 22:28:51 +0200

    UI: modernise le ticker des présents

--- noethys/Ctrl/CTRL_Ticker_presents.py
+++ noethys/Ctrl/CTRL_Ticker_presents.py
@@ -8,176 +8,196 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
+import datetime
 
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 from wx.lib.ticker import Ticker
 
-import datetime
 import GestionDB
-from Utils import UTILS_Dates
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+from Utils.UTILS_Traduction import _
 
-    
+
+def _CouleurTexteLisible(fond):
+    """Choisit noir/blanc uniquement pour un fond explicitement personnalisé."""
+    try:
+        luminance = 0.2126 * fond.Red() + 0.7152 * fond.Green() + 0.0722 * fond.Blue()
+        return wx.Colour(24, 24, 24) if luminance >= 150 else wx.Colour(248, 248, 248)
+    except Exception:
+        return UTILS_Interface.GetCouleurRole("on_surface")
+
+
 class CTRL(wx.Panel):
-    """ Delai d'actualisation en secondes """
+    """Ticker des présents, actualisé périodiquement et aligné sur le thème."""
+
     def __init__(self, parent, delai=60, listeActivites=[], fps=20, ppf=2, couleurFond=None):
         wx.Panel.__init__(self, parent, id=-1)
         self.parent = parent
         self.delai = delai
         self.listeActivites = listeActivites
-        if couleurFond != None :
-            self.SetBackgroundColour(couleurFond)
-        
+
+        if couleurFond is None:
+            fond = UTILS_Interface.GetCouleurRole("surface_container")
+            texte = UTILS_Interface.GetCouleurRole("on_surface")
+        else:
+            fond = couleurFond
+            texte = _CouleurTexteLisible(fond)
+
+        self.SetBackgroundColour(fond)
+        self.SetForegroundColour(texte)
+        self.SetMinSize((-1, UTILS_UIMetrics.panel_min_height("compact")))
+
         self.timer = wx.Timer(self, -1)
-        
         self.ticker = Ticker(self)
-        self.ticker.SetBackgroundColour(self.GetBackgroundColour())
+        self.ticker.SetBackgroundColour(fond)
+        self.ticker.SetForegroundColour(texte)
         self.ticker.SetFPS(fps)
         self.ticker.SetPPF(ppf)
-        
-        # Layout
+
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            try:
+                police.SetWeight(wx.FONTWEIGHT_SEMIBOLD)
+            except Exception:
+                pass
+            self.ticker.SetFont(police)
+        except Exception:
+            pass
+
         sizer_base = wx.BoxSizer(wx.VERTICAL)
-        sizer_base.Add(self.ticker, 1, wx.EXPAND|wx.ALL, 5)
+        sizer_base.Add(self.ticker, 1, wx.EXPAND | wx.ALL, UTILS_UIMetrics.spacing(2))
         self.SetSizer(sizer_base)
-        sizer_base.Fit(self)
-        
-        # Binds
+        self.Layout()
+
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
-        
-    
+
     def SetTexte(self, texte=u""):
         self.ticker.SetText(texte)
 
     def Stop(self):
-        """Stop moving the text"""
-        self.timer.Stop()
-        
+        """Stoppe le déplacement du texte et le rafraîchissement périodique."""
+        if self.timer.IsRunning():
+            self.timer.Stop()
+        if self.ticker.IsTicking():
+            self.ticker.Stop()
+
     def Start(self):
-        """Starts the text moving"""
+        """Démarre le rafraîchissement périodique."""
         if not self.timer.IsRunning():
             self.timer.Start(self.delai * 1000)
 
     def OnTimer(self, event):
-        self.MAJ() 
-    
+        self.MAJ()
+
     def SetActivites(self, listeActivites=[]):
         self.listeActivites = listeActivites
-        
+
     def MAJ(self):
-        try :
-            texte = self.GetTexte(self.listeActivites) 
+        try:
+            texte = self.GetTexte(self.listeActivites)
             self.SetTexte(texte)
-        except :
+        except Exception:
             texte = u""
-            
-        if len(texte) > 0 :
+
+        if len(texte) > 0:
             etat = True
-            if self.ticker.IsTicking() == False :
+            if self.ticker.IsTicking() is False:
                 self.ticker.Start()
-        else :
+        else:
             etat = False
-            if self.ticker.IsTicking() :
+            if self.ticker.IsTicking():
                 self.ticker.Stop()
-        try :
+        try:
             self.parent.AffichePresents(etat)
-        except :
+        except Exception:
             pass
-        
+
     def JoinListe(self, listeTemp=[]):
-        if len(listeTemp) > 2 :
+        if len(listeTemp) > 2:
             return _(u"%s et %s") % (u", ".join(listeTemp[:-1]), listeTemp[-1])
-        else :
-            return _(u" et ").join(listeTemp)
-        
+        return _(u" et ").join(listeTemp)
+
     def GetTexte(self, listeActivites=[]):
-        """ Récupération des données dans la base de données """
-        if len(listeActivites) == 0 : conditionActivites = "()"
-        elif len(listeActivites) == 1 : conditionActivites = "(%d)" % listeActivites[0]
-        else : conditionActivites = str(tuple(listeActivites))
-        
+        """Récupère les effectifs présents dans la base de données."""
+        if len(listeActivites) == 0:
+            conditionActivites = "()"
+        elif len(listeActivites) == 1:
+            conditionActivites = "(%d)" % listeActivites[0]
+        else:
+            conditionActivites = str(tuple(listeActivites))
+
         now = datetime.datetime.now()
         date = datetime.date(now.year, now.month, now.day)
         heure = "%02d:%02d" % (now.hour, now.minute)
-        
+
         DB = GestionDB.DB()
         req = """SELECT activites.IDactivite, activites.nom, groupes.IDgroupe, groupes.nom, groupes.ordre, COUNT(IDconso), SUM(quantite)
-        FROM consommations 
+        FROM consommations
         LEFT JOIN activites ON activites.IDactivite = consommations.IDactivite
         LEFT JOIN groupes ON groupes.IDgroupe = consommations.IDgroupe
         WHERE consommations.IDactivite IN %s
         AND date = '%s'
-        AND heure_debut <= '%s' 
+        AND heure_debut <= '%s'
         AND heure_fin >= '%s'
         AND consommations.etat IN ("reservation", "present")
         GROUP BY consommations.IDactivite, consommations.IDindividu, groupes.IDgroupe
         ORDER BY activites.nom, groupes.ordre;""" % (conditionActivites, date, heure, heure)
         DB.ExecuterReq(req)
         listeConso = DB.ResultatReq()
-        DB.Close() 
-        
-        if len(listeConso) == 0 :
+        DB.Close()
+
+        if len(listeConso) == 0:
             return u""
-        
+
         dictTemp = {}
-        listeActivites = []
-        for IDactivite, nomActivite, IDgroupe, nomGroupe, ordreGroupe, nbreConso, quantite in listeConso :
-            if (IDactivite in dictTemp) == False :
-                dictTemp[IDactivite] = {"nom" : nomActivite, "nbre" : 0, "groupes" : {} }
-                listeActivites.append(IDactivite)
-            if (IDgroupe in dictTemp[IDactivite]["groupes"]) == False  :
-                dictTemp[IDactivite]["groupes"][IDgroupe] = {"nom" : nomGroupe, "ordre" : ordreGroupe, "nbre" : 0}
+        listeActivitesTriees = []
+        for IDactivite, nomActivite, IDgroupe, nomGroupe, ordreGroupe, nbreConso, quantite in listeConso:
+            if IDactivite not in dictTemp:
+                dictTemp[IDactivite] = {"nom": nomActivite, "nbre": 0, "groupes": {}}
+                listeActivitesTriees.append(IDactivite)
+            if IDgroupe not in dictTemp[IDactivite]["groupes"]:
+                dictTemp[IDactivite]["groupes"][IDgroupe] = {"nom": nomGroupe, "ordre": ordreGroupe, "nbre": 0}
             dictTemp[IDactivite]["groupes"][IDgroupe]["nbre"] += 1
             dictTemp[IDactivite]["nbre"] += 1
-        
+
         listeTextes = []
-        for IDactivite in listeActivites :
+        for IDactivite in listeActivitesTriees:
             nomActivite = dictTemp[IDactivite]["nom"]
-            
             listeGroupes = []
-            for IDgroupe, dictGroupe in dictTemp[IDactivite]["groupes"].items() :
+            for IDgroupe, dictGroupe in dictTemp[IDactivite]["groupes"].items():
                 label = u"%d %s" % (dictGroupe["nbre"], dictGroupe["nom"])
                 listeGroupes.append((dictGroupe["ordre"], label))
-            listeGroupes.sort() 
-            groupes = []
-            for ordre, label in listeGroupes :
-                groupes.append(label)
-            
-            nbre = dictTemp[IDactivite]["nbre"]
-            if nbre == 1 :
-                temp = _(u"individu")
-            else :
-                temp = _(u"individus")
-            listeTextes.append(_(u"%d %s sur l'activité %s (%s)") % (nbre, temp, nomActivite, self.JoinListe(groupes)))
-        
-        texte = _(u"Il y a actuellement %s") % self.JoinListe(listeTextes)
-        return texte
+            listeGroupes.sort()
+            groupes = [label for ordre, label in listeGroupes]
 
-# -------------------------------------------------------------------------------------------------------------------------------------------
+            nbre = dictTemp[IDactivite]["nbre"]
+            temp = _(u"individu") if nbre == 1 else _(u"individus")
+            listeTextes.append(_(u"%d %s sur l'activité %s (%s)") % (nbre, temp, nomActivite, self.JoinListe(groupes)))
+
+        return _(u"Il y a actuellement %s") % self.JoinListe(listeTextes)
+
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1, name="test1")
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
-        self.ctrl = CTRL(panel, delai=60, listeActivites=[1,])
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        self.ctrl = CTRL(panel, delai=60, listeActivites=[1])
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        principal = wx.BoxSizer(wx.VERTICAL)
+        principal.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(principal)
         self.Layout()
-
-        # Init contrôles
-        print((self.ctrl.GetTexte([1,]),))
-        self.ctrl.MAJ() 
+        self.ctrl.MAJ()
         self.ctrl.Start()
+
 
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "OL TEST")
     app.SetTopWindow(frame_1)
     frame_1.Show()
