7b626a37cda27dbba45b1808f8306f23c7cd0ee1
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:28:49 +0200

    UI: modernise la saisie des adresses mail

--- noethys/Ctrl/CTRL_Saisie_mail.py
+++ noethys/Ctrl/CTRL_Saisie_mail.py
@@ -8,31 +8,49 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 
-    
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+
+
 class Mail(wx.TextCtrl):
+    """Saisie d'adresse mail compatible thème, DPI et grosse police."""
+
     def __init__(self, parent, size=(-1, -1)):
         wx.TextCtrl.__init__(self, parent, -1, "", size=size)
         self.parent = parent
-        self.SetToolTip(wx.ToolTip(_(u"Saisissez une adresse mail")))   
+        self._AppliqueStyle()
+        self.SetToolTip(wx.ToolTip(_(u"Saisissez une adresse mail")))
         self.Bind(wx.EVT_KILL_FOCUS, self.OnKillFocus)
-    
+
+    def _AppliqueStyle(self):
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+
+        try:
+            self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
     def OnKillFocus(self, event):
         valide, messageErreur = self.Validation()
-        if valide == False :
+        if valide is False:
             dlg = wx.MessageDialog(self, messageErreur, _(u"Erreur de saisie"), wx.OK | wx.ICON_EXCLAMATION)
             dlg.ShowModal()
             dlg.Destroy()
-        if event != None : event.Skip() 
-    
+        if event is not None:
+            event.Skip()
+
     def Validation(self):
-        # Vérifie si Email vide
         text = self.GetValue()
         if text != "":
             posAt = text.find("@")
@@ -40,43 +58,40 @@ class Mail(wx.TextCtrl):
                 message = _(u"L'adresse Email que vous avez saisie n'est pas valide !")
                 return False, message
             posPoint = text.rfind(".")
-            if posPoint < posAt :
+            if posPoint < posAt:
                 message = _(u"L'adresse Email que vous avez saisie n'est pas valide !")
                 return False, message
         return True, None
-    
+
     def SetMail(self, mail=""):
-        if mail == None : return
+        if mail is None:
+            return
         self.SetValue(mail)
-    
+
     def GetMail(self):
-        mail = self.GetValue() 
-        if mail == "" :
+        mail = self.GetValue()
+        if mail == "":
             return None
-        else:
-            return mail.strip()
-        
-        
-        
+        return mail.strip()
 
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
-        self.ctrl= Mail(panel)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        self.ctrl = Mail(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
