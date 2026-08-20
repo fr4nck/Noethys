26754623b30430d6874d405e86a7884c5e05307f
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:29:46 +0200

    UI: modernise le choix du compte bancaire

--- noethys/Ctrl/CTRL_Saisie_compte.py
+++ noethys/Ctrl/CTRL_Saisie_compte.py
@@ -9,26 +9,45 @@
 #-----------------------------------------------------------
 
 import wx
-from Ctrl import CTRL_Bouton_image
 import GestionDB
 
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 
 
 class CTRL(wx.Choice):
+    """Sélecteur de compte bancaire cohérent avec les contrôles de saisie."""
+
     def __init__(self, parent, IDcompte_bancaire=None, size=(-1, -1)):
-        wx.Choice.__init__(self, parent, -1, size=size) 
+        wx.Choice.__init__(self, parent, -1, size=size)
         self.parent = parent
         self.IDdefaut = None
-        self.MAJ() 
+        self._AppliqueStyle()
+        self.MAJ()
         self.SetID(IDcompte_bancaire)
-    
+
+    def _AppliqueStyle(self):
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+
+        try:
+            self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
     def MAJ(self):
         listeItems = self.GetListeDonnees()
-        if len(listeItems) == 0 :
-            self.Enable(False)
+        self.Enable(bool(listeItems))
         self.SetItems(listeItems)
         self.SetID(self.IDdefaut)
-    
+
     def GetListeDonnees(self):
         listeItems = []
         self.dictDonnees = {}
@@ -39,48 +58,44 @@ class CTRL(wx.Choice):
         DB.ExecuterReq(req)
         listeDonnees = DB.ResultatReq()
         DB.Close()
-        index = 0
-        for IDcompte, nom, defaut in listeDonnees :
-            self.dictDonnees[index] = { "ID" : IDcompte }
-            label = nom
-            listeItems.append(label)
-            if defaut == 1 :
+        for index, (IDcompte, nom, defaut) in enumerate(listeDonnees):
+            self.dictDonnees[index] = {"ID": IDcompte}
+            listeItems.append(nom)
+            if defaut == 1:
                 self.IDdefaut = IDcompte
-            index += 1
         return listeItems
 
     def SetID(self, ID=0):
         for index, values in self.dictDonnees.items():
-            if values["ID"] == ID :
-                 self.SetSelection(index)
+            if values["ID"] == ID:
+                self.SetSelection(index)
 
     def GetID(self):
         index = self.GetSelection()
-        if index == -1 : return None
+        if index == -1:
+            return None
         return self.dictDonnees[index]["ID"]
 
-
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1, name="panel_test")
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl1 = CTRL(panel)
         self.ctrl2 = CTRL(panel)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl1, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(self.ctrl2, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl1, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.ctrl2, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
