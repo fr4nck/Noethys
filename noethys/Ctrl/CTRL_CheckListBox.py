f8bc754d539b4fe3ff18be49a7cd3f1eff1a4d8f
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:34:03 +0200

    UI: modernise les listes à cocher

--- noethys/Ctrl/CTRL_CheckListBox.py
+++ noethys/Ctrl/CTRL_CheckListBox.py
@@ -8,74 +8,88 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
 import GestionDB
 
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+from Ctrl import CTRL_Bouton_image
 
 
 class CTRL(wx.CheckListBox):
+    """Liste à cocher utilisant les couleurs et la typographie du thème."""
+
     def __init__(self, parent):
         wx.CheckListBox.__init__(self, parent, -1)
         self.parent = parent
         self.listeDonnees = []
         self.dictDonnees = {}
+        self._AppliqueStyle()
 
-    def SetDonnees(self, listeDonnees=[], trier=False, cocher=False):
+    def _AppliqueStyle(self):
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
+    def SetDonnees(self, listeDonnees=None, trier=False, cocher=False):
+        if listeDonnees is None:
+            listeDonnees = []
         self.listeDonnees = listeDonnees
         listeLabels = []
-        for dictTemp in listeDonnees :
+        for dictTemp in listeDonnees:
             listeLabels.append((dictTemp["label"], dictTemp))
 
-        # Tri par ordre alphabétique si demandé
-        if trier == True :
+        if trier is True:
             listeLabels.sort()
 
-        # Remplissage
         self.dictDonnees = {}
         self.Clear()
-        index = 0
-        for label, dictTemp in listeLabels :
+        for index, (label, dictTemp) in enumerate(listeLabels):
             self.Append(label)
-            if cocher == True :
+            if cocher is True:
                 self.Check(index)
             self.dictDonnees[index] = dictTemp
-            index += 1
 
     def GetIDcoches(self):
         listeIDcoches = []
-        for index, dictItem in self.dictDonnees.items() :
+        for index, dictItem in self.dictDonnees.items():
             if self.IsChecked(index):
                 listeIDcoches.append(dictItem["ID"])
         return listeIDcoches
 
     def CocherTout(self):
-        for index, dictItem in self.dictDonnees.items() :
+        for index in self.dictDonnees:
             self.Check(index)
 
     def CocherRien(self):
-        for index, dictItem in self.dictDonnees.items() :
+        for index in self.dictDonnees:
             self.Check(index, False)
 
-    def SetIDcoches(self, listeIDcoches=[]):
-        for index, dictItem in self.dictDonnees.items() :
-            if dictItem["ID"] in listeIDcoches :
+    def SetIDcoches(self, listeIDcoches=None):
+        if listeIDcoches is None:
+            listeIDcoches = []
+        for index, dictItem in self.dictDonnees.items():
+            if dictItem["ID"] in listeIDcoches:
                 self.Check(index)
 
     def GetLabelsCoches(self):
-        """ Renvoie un texte de type 'label1, label2, label3, ...' """
+        """Renvoie un texte de type 'label1, label2, label3, ...'."""
         listeLabels = []
         listeID = self.GetIDcoches()
-        for index, dictItem in self.dictDonnees.items() :
-            if dictItem["ID"] in listeID :
+        for dictItem in self.dictDonnees.values():
+            if dictItem["ID"] in listeID:
                 listeLabels.append(dictItem["label"])
         return ", ".join(listeLabels)
-
-
-
 
 
 class Panel(wx.Panel):
@@ -83,44 +97,39 @@ class Panel(wx.Panel):
         wx.Panel.__init__(self, parent, id=-1, style=wx.TAB_TRAVERSAL)
         self.parent = parent
 
-        # Liste à cocher
         self.ctrl_liste = CTRL(self)
+        self.bouton_cocher_tout = CTRL_Bouton_image.CTRL(self, texte=_(u"Tout"))
+        self.bouton_cocher_rien = CTRL_Bouton_image.CTRL(self, texte=_(u"Aucun"))
 
-        # Boutons
-        self.bouton_cocher_tout = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath(u"Images/16x16/Cocher.png"), wx.BITMAP_TYPE_ANY))
-        self.bouton_cocher_rien = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath(u"Images/16x16/Decocher.png"), wx.BITMAP_TYPE_ANY))
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+        except Exception:
+            pass
 
         self.__set_properties()
         self.__do_layout()
-
-        # Binds
         self.Bind(wx.EVT_BUTTON, self.OnBoutonCocherTout, self.bouton_cocher_tout)
         self.Bind(wx.EVT_BUTTON, self.OnBoutonCocherRien, self.bouton_cocher_rien)
 
-        # Associe les fonctions du ctrl au panel
         listeFonctions = ["SetDonnees", "GetIDcoches", "CocherTout", "CocherRien", "SetIDcoches", "GetLabelsCoches"]
-        for nomFonction in listeFonctions :
-            fonction = getattr(self.ctrl_liste, nomFonction)
-            setattr(self, nomFonction, fonction)
-
+        for nomFonction in listeFonctions:
+            setattr(self, nomFonction, getattr(self.ctrl_liste, nomFonction))
 
     def __set_properties(self):
         self.bouton_cocher_tout.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour tout cocher")))
         self.bouton_cocher_rien.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour tout décocher")))
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=1, cols=2, vgap=5, hgap=5)
-        grid_sizer_base.Add(self.ctrl_liste, 1, wx.EXPAND, 0)
+        actions = wx.BoxSizer(wx.VERTICAL)
+        actions.Add(self.bouton_cocher_tout, 0, wx.EXPAND | wx.BOTTOM, UTILS_UIMetrics.spacing(1))
+        actions.Add(self.bouton_cocher_rien, 0, wx.EXPAND)
+        actions.AddStretchSpacer(1)
 
-        grid_sizer_boutons_colonnes = wx.FlexGridSizer(rows=2, cols=1, vgap= 5, hgap=5)
-        grid_sizer_boutons_colonnes.Add(self.bouton_cocher_tout, 0, 0, 0)
-        grid_sizer_boutons_colonnes.Add(self.bouton_cocher_rien, 0, 0, 0)
-        grid_sizer_base.Add(grid_sizer_boutons_colonnes, 0, wx.EXPAND, 0)
-
-        grid_sizer_base.AddGrowableRow(0)
-        grid_sizer_base.AddGrowableCol(0)
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
+        principal = wx.BoxSizer(wx.HORIZONTAL)
+        principal.Add(self.ctrl_liste, 1, wx.EXPAND)
+        principal.Add(actions, 0, wx.LEFT | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        self.SetSizer(principal)
+        self.Layout()
 
     def OnBoutonCocherTout(self, event=None):
         self.ctrl_liste.CocherTout()
@@ -129,29 +138,25 @@ class Panel(wx.Panel):
         self.ctrl_liste.CocherRien()
 
 
-# -------------------------------------------------------------------------------------------------------------------------------------------
-
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1, name="test1")
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl = Panel(panel)
         boutonTest = wx.Button(panel, -1, _(u"Test"))
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(boutonTest, 0, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(boutonTest, 0, wx.ALL, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.SetSize((900, 500))
         self.Layout()
         self.CenterOnScreen()
         self.Bind(wx.EVT_BUTTON, self.OnBoutonTest, boutonTest)
-
         self.MAJ()
         self.ctrl.CocherTout()
-
 
     def MAJ(self, IDactivite=1):
         DB = GestionDB.DB()
@@ -163,22 +168,17 @@ class MyFrame(wx.Frame):
         listeGroupes = DB.ResultatReq()
         DB.Close()
 
-        # Formatage des données
         listeDonnees = []
         for IDgroupe, IDactivite, nom in listeGroupes:
-            dictTemp = {"ID": IDgroupe, "label": nom, "IDactivite": IDactivite}
-            listeDonnees.append(dictTemp)
-
-        # Envoi des données à la liste
+            listeDonnees.append({"ID": IDgroupe, "label": nom, "IDactivite": IDactivite})
         self.ctrl.SetDonnees(listeDonnees)
 
     def OnBoutonTest(self, event):
         print(self.ctrl.GetLabelsCoches())
-        
+
 
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST")
     app.SetTopWindow(frame_1)
     frame_1.Show()
