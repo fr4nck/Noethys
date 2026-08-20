b4b79461151f65b0ab8f73ed61a94aeee2ffa363
fr4nck <f.bellardie@gmail.com>
2026-08-20 22:29:54 +0200

    UI: modernise les pièces jointes email

--- noethys/Ctrl/CTRL_Pieces_jointes_emails.py
+++ noethys/Ctrl/CTRL_Pieces_jointes_emails.py
@@ -8,111 +8,139 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
+import os
+
+import wx
 
 import Chemins
 from Utils import UTILS_Adaptations
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 from Utils.UTILS_Traduction import _
-import wx
-from Ctrl import CTRL_Bouton_image
-import os
-import GestionDB
 
-if wx.VERSION < (2, 9, 0, 0) :
+if wx.VERSION < (2, 9, 0, 0):
     from Outils import ultimatelistctrl as ULC
-else :
+else:
     from wx.lib.agw import ultimatelistctrl as ULC
 
 
 class CTRL(ULC.UltimateListCtrl):
+    """Liste des pièces jointes avec icônes et états compatibles thèmes."""
+
     def __init__(self, parent, listePieces=[]):
-        ULC.UltimateListCtrl.__init__(self, parent, -1, 
-                                                #agwStyle=ULC.ULC_SINGLE_SEL | ULC.ULC_REPORT |  ULC.ULC_NO_HEADER | ULC.ULC_HAS_VARIABLE_ROW_HEIGHT,
-                                                agwStyle=wx.LC_LIST | ULC.ULC_SINGLE_SEL,
-                                                )
+        ULC.UltimateListCtrl.__init__(
+            self,
+            parent,
+            -1,
+            agwStyle=wx.LC_LIST | ULC.ULC_SINGLE_SEL,
+        )
         self.parent = parent
         self.listePieces = listePieces
-        
-        # Liste images
+        self._taille_icone = UTILS_UIMetrics.icon_size("inline")
+
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+        self.SetMinSize((UTILS_UIMetrics.px(260), UTILS_UIMetrics.panel_min_height("secondary")))
+        self.SetToolTip(wx.ToolTip(_(u"Pièces jointes. Clic droit pour ajouter ou supprimer un fichier.")))
+
         self.dictImages = {}
-        il = wx.ImageList(16, 16, True)
-        listeExtensions = ["bmp", "doc", "docx", "gif", "jpeg", "jpg", "pdf", "png", "tous", "xls", "xlsx", "zip",]
-        for extension in listeExtensions :
-            self.dictImages[extension] = il.Add(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Fichier_%s.png" % extension), wx.BITMAP_TYPE_ANY))
+        il = wx.ImageList(self._taille_icone, self._taille_icone, True)
+        listeExtensions = ["bmp", "doc", "docx", "gif", "jpeg", "jpg", "pdf", "png", "tous", "xls", "xlsx", "zip"]
+        for extension in listeExtensions:
+            chemin = Chemins.GetStaticIconPath("Images/16x16/Fichier_%s.png" % extension, taille=self._taille_icone)
+            bitmap = wx.Bitmap(chemin, wx.BITMAP_TYPE_ANY)
+            if bitmap.IsOk() and (bitmap.GetWidth() != self._taille_icone or bitmap.GetHeight() != self._taille_icone):
+                image = bitmap.ConvertToImage().Scale(self._taille_icone, self._taille_icone, wx.IMAGE_QUALITY_HIGH)
+                bitmap = wx.Bitmap(image)
+            self.dictImages[extension] = il.Add(bitmap)
         self.AssignImageList(il, wx.IMAGE_LIST_SMALL)
-            
+
         self.Bind(ULC.EVT_LIST_ITEM_RIGHT_CLICK, self.OnContextMenu)
         self.Bind(wx.EVT_RIGHT_DOWN, self.OnContextMenu)
-
+        self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
         self.MAJ()
-    
+
     def MAJ(self):
-        self.DeleteAllItems() 
-        index = 0
-        for dictFichier in self.listePieces :
+        self.DeleteAllItems()
+        for index, dictFichier in enumerate(self.listePieces):
             nomFichier = os.path.basename(dictFichier["nom"])
             extension = dictFichier["extension"]
             taille = dictFichier["taille"]
             obligatoire = dictFichier["obligatoire"]
 
-            if taille != None :
-                if taille >= 1000000 :
-                    texteTaille = "%s Mo" % (taille/1000000)
-                else :
-                    texteTaille = "%s Ko" % (taille/1000)
+            if taille is not None:
+                if taille >= 1000000:
+                    texteTaille = u"%.1f Mo" % (taille / 1000000.0)
+                else:
+                    texteTaille = u"%.0f Ko" % (taille / 1000.0)
                 label = u"%s (%s)" % (nomFichier, texteTaille)
-            else :
+            else:
                 label = nomFichier
-                
-            if extension in self.dictImages :
-                bmp = self.dictImages[extension]
-            else:
-                bmp = self.dictImages["tous"]
-                
+
+            bmp = self.dictImages.get(extension, self.dictImages["tous"])
             self.InsertImageStringItem(index, label, bmp)
-            self.SetItemData(index, dictFichier)            
-            
-            if obligatoire == True :
+            self.SetItemData(index, dictFichier)
+
+            if obligatoire is True:
                 self.EnableItem(index, enable=False)
-                self.SetItemTextColour(index, wx.Colour(150, 150, 150))
-                
-            index += 1
-            
-        try :
-            if len(self.listePieces) > 0 :
-                self.parent.box_pieces_staticbox.SetLabel(u"Pièces jointes communes (%d)" % len(self.listePieces))
+                self.SetItemTextColour(index, UTILS_Interface.GetCouleurRole("on_surface_variant"))
+
+        try:
+            if len(self.listePieces) > 0:
+                self.parent.box_pieces_staticbox.SetLabel(_(u"Pièces jointes communes (%d)") % len(self.listePieces))
             else:
-                self.parent.box_pieces_staticbox.SetLabel(u"Pièces jointes communes")
-        except :
+                self.parent.box_pieces_staticbox.SetLabel(_(u"Pièces jointes communes"))
+        except Exception:
             pass
-    
+
+    def OnKeyDown(self, event):
+        code = event.GetKeyCode()
+        if code in (wx.WXK_DELETE, wx.WXK_BACK):
+            self.Supprimer(None)
+            return
+        if code in (wx.WXK_INSERT,):
+            self.Ajouter(None)
+            return
+        event.Skip()
+
+    def _BitmapMenu(self, image):
+        chemin = Chemins.GetStaticIconPath(image, taille=UTILS_UIMetrics.icon_size("compact"))
+        return wx.Bitmap(chemin, wx.BITMAP_TYPE_ANY)
+
     def OnContextMenu(self, event):
-        """Ouverture du menu contextuel """
-        item, flags = self.HitTest((event.GetX(), event.GetY()))
+        try:
+            point = (event.GetX(), event.GetY())
+            item, flags = self.HitTest(point)
+        except Exception:
+            item, flags = wx.NOT_FOUND, 0
+
         if item != wx.NOT_FOUND and flags & wx.LIST_HITTEST_ONITEM:
             self.Select(item)
             noSelection = False
         else:
             noSelection = True
             self.ToutDeselectionner()
-                
-        # Création du menu contextuel
-        menuPop = UTILS_Adaptations.Menu()
 
-        # Item Ajouter
-        item = wx.MenuItem(menuPop, 10, _(u"Ajouter"))
-        bmp = wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Ajouter.png"), wx.BITMAP_TYPE_PNG)
-        item.SetBitmap(bmp)
-        menuPop.AppendItem(item)
+        menuPop = UTILS_Adaptations.Menu()
+        item_ajouter = wx.MenuItem(menuPop, 10, _(u"Ajouter une pièce jointe"))
+        item_ajouter.SetBitmap(self._BitmapMenu("Images/16x16/Ajouter.png"))
+        menuPop.AppendItem(item_ajouter)
         self.Bind(wx.EVT_MENU, self.Ajouter, id=10)
-        
-        # Item Supprimer
-        item = wx.MenuItem(menuPop, 30, _(u"Supprimer"))
-        bmp = wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Supprimer.png"), wx.BITMAP_TYPE_PNG)
-        item.SetBitmap(bmp)
-        menuPop.AppendItem(item)
+
+        item_supprimer = wx.MenuItem(menuPop, 30, _(u"Supprimer la pièce jointe"))
+        item_supprimer.SetBitmap(self._BitmapMenu("Images/16x16/Supprimer.png"))
+        menuPop.AppendItem(item_supprimer)
         self.Bind(wx.EVT_MENU, self.Supprimer, id=30)
-        if noSelection == True : item.Enable(False)
-                        
+        if noSelection is True:
+            item_supprimer.Enable(False)
+
         self.PopupMenu(menuPop)
         menuPop.Destroy()
 
@@ -123,78 +151,80 @@ class CTRL(ULC.UltimateListCtrl):
             index = self.GetNextSelected(index)
 
     def Ajouter(self, event):
-        # Demande l'emplacement du fichier à joindre
         standardPath = wx.StandardPaths.Get()
         rep = standardPath.GetDocumentsDir()
-        dlg = wx.FileDialog(self, message=_(u"Veuillez sélectionner le ou les fichiers à joindre"), defaultDir=rep, defaultFile="", style=wx.FD_OPEN|wx.FD_MULTIPLE)
-        if dlg.ShowModal() == wx.ID_OK:
+        dlg = wx.FileDialog(
+            self,
+            message=_(u"Veuillez sélectionner le ou les fichiers à joindre"),
+            defaultDir=rep,
+            defaultFile="",
+            style=wx.FD_OPEN | wx.FD_MULTIPLE,
+        )
+        try:
+            if dlg.ShowModal() != wx.ID_OK:
+                return
             chemins = dlg.GetPaths()
-        else:
-            return
-        dlg.Destroy()
-        for fichier in chemins :
+        finally:
+            dlg.Destroy()
+
+        for fichier in chemins:
             valide = True
-            
-            for dictFichier in self.listePieces :
-                if fichier == dictFichier["nom"] :
-                    dlg = wx.MessageDialog(self, _(u"Le fichier '%s' est déjà dans la liste !") % os.path.basename(fichier), "Erreur", wx.OK| wx.ICON_EXCLAMATION)  
+            for dictFichier in self.listePieces:
+                if fichier == dictFichier["nom"]:
+                    dlg = wx.MessageDialog(
+                        self,
+                        _(u"Le fichier '%s' est déjà dans la liste !") % os.path.basename(fichier),
+                        _(u"Erreur"),
+                        wx.OK | wx.ICON_EXCLAMATION,
+                    )
                     dlg.ShowModal()
                     dlg.Destroy()
                     valide = False
-            
-            if valide == True :
+            if valide is True:
                 extension = fichier.split('.')[-1].lower()
                 taille = os.path.getsize(fichier)
-                dictFichier = {"nom":fichier, "extension":extension, "taille":taille, "obligatoire":False}
-                self.listePieces.append(dictFichier)
+                self.listePieces.append({"nom": fichier, "extension": extension, "taille": taille, "obligatoire": False})
         self.MAJ()
 
     def Supprimer(self, event):
-        if self.GetSelectedItemCount() == 0 :
-            dlg = wx.MessageDialog(self, _(u"Vous n'avez sélectionné aucune pièce jointe à enlever de la liste !"), "Erreur", wx.OK| wx.ICON_EXCLAMATION)  
-            dlg.ShowModal()
-            dlg.Destroy()
+        if self.GetSelectedItemCount() == 0:
+            if event is not None:
+                dlg = wx.MessageDialog(self, _(u"Vous n'avez sélectionné aucune pièce jointe à enlever de la liste !"), _(u"Erreur"), wx.OK | wx.ICON_EXCLAMATION)
+                dlg.ShowModal()
+                dlg.Destroy()
             return
         index = self.GetFirstSelected()
-        if self.listePieces[index]["obligatoire"] == True :
-            dlg = wx.MessageDialog(self, _(u"Vous ne pouvez pas désélectionner cette pièce !"), "Erreur", wx.OK| wx.ICON_EXCLAMATION)  
+        if self.listePieces[index]["obligatoire"] is True:
+            dlg = wx.MessageDialog(self, _(u"Vous ne pouvez pas désélectionner cette pièce !"), _(u"Erreur"), wx.OK | wx.ICON_EXCLAMATION)
             dlg.ShowModal()
             dlg.Destroy()
             return
         self.listePieces.pop(index)
         self.MAJ()
-    
+
     def SetPieces(self, listePieces=[]):
         self.listePieces = listePieces
-        self.MAJ() 
-        
+        self.MAJ()
 
-# ----------------------------------------------------------------------------------------------------------------------        
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
-
-        listePieces = []
-        for x in range(0, 1) :
-            listePieces.append(
-                {"nom":_(u"Facture"), "extension":"pdf", "taille":None, "obligatoire":True},
-                )
-
+        listePieces = [{"nom": _(u"Facture"), "extension": "pdf", "taille": None, "obligatoire": True}]
         self.ctrl = CTRL(panel, listePieces=listePieces)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        principal = wx.BoxSizer(wx.VERTICAL)
+        principal.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(principal)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
