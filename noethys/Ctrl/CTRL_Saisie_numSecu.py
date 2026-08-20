67c79a9a6ace285193659aa7b37abd83ddf01d47
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:32:10 +0200

    UI: modernise la saisie du numéro de sécurité sociale

--- noethys/Ctrl/CTRL_Saisie_numSecu.py
+++ noethys/Ctrl/CTRL_Saisie_numSecu.py
@@ -8,32 +8,38 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 import wx.lib.masked as masked
 
-    
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+
+
 class NumSecu(wx.Panel):
+    """Saisie du NIR avec validation et indicateur sémantique scalable."""
+
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, id=-1, style=wx.TAB_TRAVERSAL)
         self.parent = parent
-        self.ctrl_numsecu = masked.TextCtrl(self, -1, "", style=wx.TE_CENTRE, mask = "# ## ## #N ### ### ##") 
-        self.image_numsecu = wx.StaticBitmap(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Interdit.png"), wx.BITMAP_TYPE_PNG), size=(16, 16))
+        self.ctrl_numsecu = masked.TextCtrl(
+            self,
+            -1,
+            "",
+            style=wx.TE_CENTRE,
+            mask="# ## ## #N ### ### ##",
+        )
+        self.indicateur = wx.StaticText(self, -1, u"–", style=wx.ALIGN_CENTER)
         self.remplissageEnCours = False
-        
+
         self.__set_properties()
         self.__do_layout()
-        
         self.ctrl_numsecu.Bind(wx.EVT_KILL_FOCUS, self.OnKillFocus)
 
     def __set_properties(self):
         texteNumSecu = u"""
-Attention, le numéro de sécurité sociale est considéré comme une donnée 
-sensible par la CNIL. Assurez-vous que vous êtes autorisé à saisir cette 
+Attention, le numéro de sécurité sociale est considéré comme une donnée
+sensible par la CNIL. Assurez-vous que vous êtes autorisé à saisir cette
 information dans votre fichier de données.
 
 Numéro de sécurité sociale : A BB CC DD EEE FFF GG
@@ -47,138 +53,162 @@ FFF : Numéro d'ordre INSEE
 GG : Clé
         """
         self.ctrl_numsecu.SetToolTip(wx.ToolTip(texteNumSecu))
-        self.ctrl_numsecu.SetMinSize((170, -1))
+
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.ctrl_numsecu.SetFont(police)
+
+            police_etat = wx.Font(police)
+            police_etat.SetWeight(wx.FONTWEIGHT_BOLD)
+            police_etat.SetPointSize(max(police.GetPointSize() + 2, 10))
+            self.indicateur.SetFont(police_etat)
+        except Exception:
+            pass
+
+        try:
+            largeur = max(
+                UTILS_UIMetrics.px(196),
+                self.ctrl_numsecu.GetTextExtent("1 99 99 99 999 999 99")[0] + UTILS_UIMetrics.spacing(4),
+            )
+            hauteur = UTILS_UIMetrics.action_target("compact")
+            self.ctrl_numsecu.SetMinSize((largeur, hauteur))
+            self.indicateur.SetMinSize((hauteur, hauteur))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+            self.ctrl_numsecu.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.ctrl_numsecu.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
+        self._AfficheEtat(None)
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=1, cols=2, vgap=1, hgap=1)
-        grid_sizer_base.Add(self.ctrl_numsecu, 1, wx.EXPAND|wx.ALL, 0)
-        grid_sizer_base.Add(self.image_numsecu, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 0)
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.ctrl_numsecu, 1, wx.EXPAND)
+        sizer.Add(self.indicateur, 0, wx.LEFT | wx.ALIGN_CENTER_VERTICAL, UTILS_UIMetrics.spacing(1))
+        self.SetSizer(sizer)
+        self.Fit()
         self.Layout()
-        self.grid_sizer_base = grid_sizer_base
-        
+
+    def _AfficheEtat(self, validation):
+        try:
+            if validation is True:
+                self.indicateur.SetLabel(u"✓")
+                self.indicateur.SetForegroundColour(UTILS_Interface.GetCouleurRole("success"))
+                self.indicateur.SetToolTip(wx.ToolTip(_(u"Numéro de sécurité sociale cohérent")))
+            elif validation is False:
+                self.indicateur.SetLabel(u"!")
+                self.indicateur.SetForegroundColour(UTILS_Interface.GetCouleurRole("danger"))
+                self.indicateur.SetToolTip(wx.ToolTip(_(u"Numéro de sécurité sociale à vérifier")))
+            else:
+                self.indicateur.SetLabel(u"–")
+                self.indicateur.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface_variant"))
+                self.indicateur.SetToolTip(wx.ToolTip(_(u"Aucun numéro renseigné")))
+            self.indicateur.Refresh()
+            self.Layout()
+        except Exception:
+            pass
+
     def SetValue(self, numSecu=""):
-        if numSecu == None : return
+        if numSecu is None:
+            return
         self.remplissageEnCours = True
-        try :
+        try:
             self.ctrl_numsecu.SetValue(numSecu)
-        except :
+        except Exception:
             pass
         self.TestValidite(avecMessagesErreur=False)
         self.remplissageEnCours = False
-    
+
     def GetValue(self):
         return self.ctrl_numsecu.GetValue()
 
     def OnKillFocus(self, event):
-        """ Verifie la validite du numero de securite sociale """
-        self.TestValidite() 
-        if event != None :
-            event.Skip() 
-        
+        self.TestValidite()
+        if event is not None:
+            event.Skip()
+
     def TestValidite(self, avecMessagesErreur=True):
         texte = self.ctrl_numsecu.GetValue()
         sexe = self.parent.ctrl_civilite.GetSexe()
         datenaiss = self.parent.ctrl_datenaiss.GetValue()
         cp_naiss = self.parent.ctrl_adressenaiss.GetValueCP()
         validation, message = self.ValideNumSecu(texte, sexe, datenaiss, cp_naiss)
-        
-        # Message si numéro de sécu erroné
-        if validation == False :
-            self.image_numsecu.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Interdit.png"), wx.BITMAP_TYPE_PNG))
-            if self.remplissageEnCours == False and avecMessagesErreur == True:
-                wx.MessageBox(message, _(u"Numéro de sécurité sociale erroné"))
-            
-        # Pas de num sécu saisi
-        if validation == None : 
-            self.image_numsecu.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Interdit.png"), wx.BITMAP_TYPE_PNG))
-        
-        #Le numéro de sécu est bon
-        if validation == True :
-            self.image_numsecu.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Ok.png"), wx.BITMAP_TYPE_PNG))
-        
-        # Actualisation de l'affichage pour le bitmap
-        self.grid_sizer_base.Layout()
+
+        self._AfficheEtat(validation)
+        if validation is False and self.remplissageEnCours is False and avecMessagesErreur is True:
+            dlg = wx.MessageDialog(self, message, _(u"Numéro de sécurité sociale erroné"), wx.OK | wx.ICON_EXCLAMATION)
+            dlg.ShowModal()
+            dlg.Destroy()
 
     def ValideNumSecu(self, texte, sexe, date_naiss, cp_naiss):
-        # On vérifie que tous les chiffres ont été donnés
         texteSansEsp = ""
         for lettre in texte:
             if lettre != " ":
-                if lettre in "ABab" :
+                if lettre in "ABab":
                     lettre = "0"
-                texteSansEsp = texteSansEsp + lettre
+                texteSansEsp += lettre
 
         nbreChiffres = len(texteSansEsp)
-        if nbreChiffres == 0 :
+        if nbreChiffres == 0:
             return None, ""
-        if nbreChiffres < 15 :
+        if nbreChiffres < 15:
             message = _(u"Il manque ") + str(15 - nbreChiffres) + _(u" chiffre(s) au numéro de sécurité sociale que vous venez de saisir. Veuillez le vérifier.")
             return False, message
+
         if nbreChiffres == 15:
-            
-            # Vérification avec la civilite
-            if sexe == "M" :
-                if int(texteSansEsp[0]) != 1:
-                    message = _(u"Le numéro de sécurité sociale ne correspond pas à la civilité de la personne (le premier chiffre devrait être 1).")
-                    return False, message
-            if sexe == "F" :
-                if int(texteSansEsp[0]) != 2:
-                    message = _(u"Le numéro de sécurité sociale ne correspond pas à la civilité de la personne (le premier chiffre devrait être 2).")
-                    return False, message
-                    
-            # Vérification avec la date de naissance
-            if date_naiss != u"  /  /    " :
+            if sexe == "M" and int(texteSansEsp[0]) != 1:
+                message = _(u"Le numéro de sécurité sociale ne correspond pas à la civilité de la personne (le premier chiffre devrait être 1).")
+                return False, message
+            if sexe == "F" and int(texteSansEsp[0]) != 2:
+                message = _(u"Le numéro de sécurité sociale ne correspond pas à la civilité de la personne (le premier chiffre devrait être 2).")
+                return False, message
+
+            if date_naiss != u"  /  /    ":
                 mois = str(date_naiss[3:5])
                 annee = str(date_naiss[8:10])
-
                 if annee != str(texteSansEsp[1:3]):
                     message = _(u"Le numéro de sécurité sociale ne correspond pas à l'année de naissance de la personne.")
                     return False, message
-                elif mois != str(texteSansEsp[3:5]):
+                if mois != str(texteSansEsp[3:5]):
                     message = _(u"Le numéro de sécurité sociale ne correspond pas au mois de naissance de la personne.")
                     return False, message
-                        
-            # Vérification avec le département de naissance
-            if cp_naiss != u"     " and cp_naiss != None :
+
+            if cp_naiss != u"     " and cp_naiss is not None:
                 dep = cp_naiss[0:2]
                 if str(dep) != str(texteSansEsp[5:7]):
                     message = _(u"Le numéro de sécurité sociale ne correspond pas au lieu de naissance de la personne.")
                     return False, message
-            
-            # Vérification de la clé
-            cle = int((texteSansEsp[13:15]))
+
+            cle = int(texteSansEsp[13:15])
             cle_calculee = 97 - (int(texteSansEsp[:13]) % 97)
-            if cle != cle_calculee :
+            if cle != cle_calculee:
                 message = _(u"La clé du numéro de sécurité sociale ne semble pas cohérente. \nD'après mes calculs, la bonne clé devrait être %02d. \n\nVeuillez vérifier votre saisie...") % cle_calculee
                 return False, message
-            
-            # Le num de sécu est ok
+
             return True, ""
-        
-        
-        
+
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl = NumSecu(panel)
         self.bouton = wx.Button(panel, -1, _(u"Test"))
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(self.bouton, 0, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.bouton, 0, wx.ALL, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
