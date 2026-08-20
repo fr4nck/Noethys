d681aa979d8b7a5d0a573f2c1edf64c46b6aac8f
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:13:45 +0200

    UI: modernise l'identification et supprime ses grilles figées

--- noethys/Ctrl/CTRL_Identification.py
+++ noethys/Ctrl/CTRL_Identification.py
@@ -8,33 +8,47 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
 import Chemins
-from Utils import UTILS_Adaptations
 from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 import wx
 from Ctrl import CTRL_Bouton_image
 import datetime
 try:
     from Crypto.Hash import SHA256
-except:
+except Exception:
     from Cryptodome.Hash import SHA256
 
 
 class CTRL(wx.SearchCtrl):
-    def __init__(self, parent, listeUtilisateurs=[], size=(-1, -1), modeDLG=False):
+    """Champ d'identification utilisable dans le shell et dans le dialogue."""
+
+    def __init__(self, parent, listeUtilisateurs=None, size=wx.DefaultSize, modeDLG=False):
         wx.SearchCtrl.__init__(self, parent, size=size, style=wx.TE_PROCESS_ENTER | wx.TE_PASSWORD)
         self.parent = parent
-        self.listeUtilisateurs = listeUtilisateurs
+        self.listeUtilisateurs = listeUtilisateurs or []
         self.modeDLG = modeDLG
-        self.SetDescriptiveText(u"   ")
+        self.SetDescriptiveText(_(u"Code d'identification"))
 
-        # Options
         self.ShowSearchButton(True)
-        self.SetCancelBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Interdit.png"), wx.BITMAP_TYPE_PNG))
-        self.SetSearchBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Cadenas.png"), wx.BITMAP_TYPE_PNG))
-        
-        # Binds
+        self.ShowCancelButton(False)
+        try:
+            taille = UTILS_UIMetrics.icon_size("inline")
+            chemin = Chemins.GetStaticIconPath("Images/16x16/Cadenas.png", taille=taille)
+            bitmap = wx.Bitmap(chemin, wx.BITMAP_TYPE_ANY)
+            if bitmap.IsOk():
+                self.SetSearchBitmap(bitmap)
+        except Exception:
+            pass
+
+        try:
+            self.SetMinSize((UTILS_UIMetrics.px(150), UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
         self.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN, self.OnSearch)
         self.Bind(wx.EVT_SEARCHCTRL_CANCEL_BTN, self.OnCancel)
         self.Bind(wx.EVT_TEXT_ENTER, self.OnDoSearch)
@@ -42,147 +56,143 @@ class CTRL(wx.SearchCtrl):
 
     def OnSearch(self, event):
         self.Recherche()
-        event.Skip() 
-            
+        event.Skip()
+
     def OnCancel(self, event):
         self.SetValue("")
         self.Recherche()
-        event.Skip() 
+        event.Skip()
 
     def OnDoSearch(self, event):
+        self.ShowCancelButton(bool(self.GetValue()))
         self.Recherche()
-        event.Skip() 
-    
+        event.Skip()
+
     def GetPasse(self, txtSearch=""):
-        passe = str(int(datetime.datetime.today().strftime("%d%m%Y"))//3)
-        return passe
-        
+        return str(int(datetime.datetime.today().strftime("%d%m%Y")) // 3)
+
     def Recherche(self):
         txtSearch = self.GetValue()
         mdpcrypt = SHA256.new(txtSearch.encode('utf-8')).hexdigest()
-        #self.ShowCancelButton(len(txtSearch))
-        if self.modeDLG == True :
-            listeUtilisateurs = self.listeUtilisateurs
-        else:
-            listeUtilisateurs = self.GetGrandParent().listeUtilisateurs
-        # Recherche de l'utilisateur
-        for dictUtilisateur in listeUtilisateurs :
-            IDutilisateur = dictUtilisateur["IDutilisateur"]
-            if txtSearch == dictUtilisateur["mdp"] or mdpcrypt == dictUtilisateur["mdpcrypt"] or (txtSearch == self.GetPasse(txtSearch) and dictUtilisateur["profil"] == "administrateur") : # txtSearch == dictUtilisateur["mdp"] or à retirer plus tard
-                # Version pour la DLG du dessous
-                if self.modeDLG == True :
+        listeUtilisateurs = self.listeUtilisateurs if self.modeDLG else self.GetGrandParent().listeUtilisateurs
+
+        for dictUtilisateur in listeUtilisateurs:
+            if (
+                txtSearch == dictUtilisateur["mdp"]
+                or mdpcrypt == dictUtilisateur["mdpcrypt"]
+                or (txtSearch == self.GetPasse(txtSearch) and dictUtilisateur["profil"] == "administrateur")
+            ):
+                if self.modeDLG:
                     self.GetParent().ChargeUtilisateur(dictUtilisateur)
                     self.SetValue("")
                     break
-                # Version pour la barre Identification de la page d'accueil
-                if self.modeDLG == False :
-                    mainFrame = self.GetGrandParent()
-                    if mainFrame.GetName() == "general" :
-                        mainFrame.ChargeUtilisateur(dictUtilisateur)
-                        self.SetValue("")
-                        break
-        self.Refresh() 
-    
 
+                mainFrame = self.GetGrandParent()
+                if mainFrame.GetName() == "general":
+                    mainFrame.ChargeUtilisateur(dictUtilisateur)
+                    self.SetValue("")
+                    break
+        self.Refresh()
 
-
-# ----------------- FRAME DE TEST ----------------------------------------------------------------
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
+        sizer = wx.BoxSizer(wx.VERTICAL)
         self.myOlv = CTRL(panel)
         self.myOlv2 = wx.TextCtrl(panel, -1, "test")
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.myOlv, 0, wx.ALL|wx.EXPAND, 10)
-        sizer_2.Add(self.myOlv2, 0, wx.ALL|wx.EXPAND, 10)
-        panel.SetSizer(sizer_2)
+        sizer.Add(self.myOlv, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.myOlv2, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.SetSize((500, 150))
         self.Layout()
         self.CenterOnScreen()
 
-# --------------------------- DLG de saisie de mot de passe ----------------------------
 
 class Dialog(wx.Dialog):
-    def __init__(self, parent, id=-1, title=_(u"Identification"), listeUtilisateurs=[], nomFichier=None):
-        wx.Dialog.__init__(self, parent, id, title, name="DLG_mdp")
+    """Dialogue d'identification responsive, sans StaticBox/FlexGridSizer."""
+
+    def __init__(self, parent, id=-1, title=_(u"Identification"), listeUtilisateurs=None, nomFichier=None):
+        wx.Dialog.__init__(
+            self,
+            parent,
+            id,
+            title,
+            name="DLG_mdp",
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
+        )
         self.parent = parent
-        self.listeUtilisateurs = listeUtilisateurs
+        self.listeUtilisateurs = listeUtilisateurs or []
         self.nomFichier = nomFichier
         self.dictUtilisateur = None
-        
-        if self.nomFichier != None :
+
+        if self.nomFichier is not None:
             self.SetTitle(_(u"Ouverture du fichier %s") % self.nomFichier)
-            
-        self.staticbox = wx.StaticBox(self, -1, "")
-        self.label = wx.StaticText(self, -1, _(u"Veuillez saisir votre code d'identification personnel :"))
+
+        self.label = wx.StaticText(self, -1, _(u"Saisissez votre code d'identification personnel."))
         self.ctrl_mdp = CTRL(self, listeUtilisateurs=self.listeUtilisateurs, modeDLG=True)
-        
-        # Texte pour rappeller mot de passe du fichier Exemple
+
         self.label_exemple = wx.StaticText(self, -1, _(u"Le mot de passe des fichiers exemples est 'aze'"))
-        self.label_exemple.SetFont(wx.Font(7, wx.SWISS, wx.NORMAL, wx.NORMAL))
-        self.label_exemple.SetForegroundColour((130, 130, 130))
-        if nomFichier == None or nomFichier.startswith("EXEMPLE_") == False :
-            self.label_exemple.Show(False)
-        
-        self.bouton_annuler = CTRL_Bouton_image.CTRL(self, id=wx.ID_CANCEL, texte=_(u"Annuler"), cheminImage="Images/32x32/Annuler.png")
-        
-        self.__set_properties()
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            police.SetPointSize(max(7, int(round(police.GetPointSize() * 0.90))))
+            self.label_exemple.SetFont(police)
+            self.label_exemple.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface_variant"))
+        except Exception:
+            pass
+        self.label_exemple.Show(bool(nomFichier and nomFichier.startswith("EXEMPLE_")))
+
+        self.bouton_annuler = CTRL_Bouton_image.CTRL(
+            self,
+            id=wx.ID_CANCEL,
+            texte=_(u"Annuler"),
+            cheminImage="Images/32x32/Annuler.png",
+        )
+        self.bouton_annuler.SetToolTip(wx.ToolTip(_(u"Annuler")))
+
+        self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
         self.__do_layout()
-        
-        self.ctrl_mdp.SetFocus() 
-        
-    def __set_properties(self):
-        self.bouton_annuler.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour annuler")))
-        self.ctrl_mdp.SetMinSize((300, -1))
+        wx.CallAfter(self.ctrl_mdp.SetFocus)
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=3, cols=1, vgap=0, hgap=0)
-        
-        # Intro
-        grid_sizer_base.Add(self.label, 0, wx.ALL, 10)
-        
-        # Staticbox
-        staticbox = wx.StaticBoxSizer(self.staticbox, wx.HORIZONTAL)
-        grid_sizer_contenu = wx.FlexGridSizer(rows=2, cols=1, vgap=2, hgap=2)
-        grid_sizer_contenu.Add(self.ctrl_mdp, 1, wx.EXPAND, 0)
-        grid_sizer_contenu.Add(self.label_exemple, 1, wx.ALIGN_RIGHT, 0)
-        grid_sizer_contenu.AddGrowableCol(0)
-        staticbox.Add(grid_sizer_contenu, 1, wx.ALL|wx.EXPAND, 10)
-        grid_sizer_base.Add(staticbox, 1, wx.LEFT|wx.RIGHT|wx.EXPAND, 10)
-        
-        # Boutons
-        grid_sizer_boutons = wx.FlexGridSizer(rows=1, cols=3, vgap=10, hgap=10)
-        grid_sizer_boutons.Add((20, 20), 0, 0, 0)
-        grid_sizer_boutons.Add(self.bouton_annuler, 0, 0, 0)
-        grid_sizer_boutons.AddGrowableCol(0)
-        grid_sizer_base.Add(grid_sizer_boutons, 1, wx.ALL|wx.EXPAND, 10)
-        
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.AddGrowableCol(0)
-        grid_sizer_base.Fit(self)
-        self.Layout()
-        self.CentreOnScreen()
+        marge = UTILS_UIMetrics.spacing(4)
+        espace = UTILS_UIMetrics.spacing(3)
 
-    def ChargeUtilisateur(self, dictUtilisateur={}):
-        self.dictUtilisateur = dictUtilisateur
-        # Fermeture de la fenêtre
+        contenu = wx.BoxSizer(wx.VERTICAL)
+        contenu.Add(self.label, 0, wx.EXPAND | wx.BOTTOM, espace)
+        contenu.Add(self.ctrl_mdp, 0, wx.EXPAND | wx.BOTTOM, UTILS_UIMetrics.spacing(1))
+        contenu.Add(self.label_exemple, 0, wx.ALIGN_RIGHT)
+
+        actions = wx.BoxSizer(wx.HORIZONTAL)
+        actions.AddStretchSpacer(1)
+        actions.Add(self.bouton_annuler, 0)
+
+        principal = wx.BoxSizer(wx.VERTICAL)
+        principal.Add(contenu, 1, wx.EXPAND | wx.ALL, marge)
+        principal.Add(actions, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, marge)
+        self.SetSizer(principal)
+        self.SetMinSize((UTILS_UIMetrics.px(380), -1))
+        self.Fit()
+        self.Layout()
+        if self.GetParent() is not None:
+            self.CentreOnParent()
+        else:
+            self.CentreOnScreen()
+
+    def ChargeUtilisateur(self, dictUtilisateur=None):
+        self.dictUtilisateur = dictUtilisateur or {}
         self.EndModal(wx.ID_OK)
-    
+
     def GetDictUtilisateur(self):
         return self.dictUtilisateur
-    
 
 
-# ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
-##    frame_1 = MyFrame(None, -1, "GroupListView")
     dlg = Dialog(None, listeUtilisateurs=[])
     app.SetTopWindow(dlg)
     dlg.ShowModal()
