9d2fdcf17b63237fdc6227110b30e74171692529
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:22:26 +0200

    UI: rend la saisie d'heure compatible DPI et thèmes

--- noethys/Ctrl/CTRL_Saisie_heure.py
+++ noethys/Ctrl/CTRL_Saisie_heure.py
@@ -8,70 +8,93 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 import wx.lib.masked as masked
 import datetime
 
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 
 
-    
 class Heure(masked.TextCtrl):
-    def __init__(self, parent, heure_max=24, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.TE_CENTRE):
-        masked.TextCtrl.__init__(self, parent, id=id, value="", pos=pos, size=size, style=style, mask="##:##", validRegex="[0-2][0-9]:[0-5][0-9]") # formatcodes="T",
+    """Saisie d'heure compacte, lisible et compatible grosse police/DPI."""
+
+    def __init__(self, parent, heure_max=24, id=wx.ID_ANY, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, style=wx.TE_CENTRE):
+        masked.TextCtrl.__init__(
+            self,
+            parent,
+            id=id,
+            value="",
+            pos=pos,
+            size=size,
+            style=style,
+            mask="##:##",
+            validRegex="[0-2][0-9]:[0-5][0-9]",
+        )
         self.parent = parent
         self.heure_max = heure_max
-        self.SetMinSize((60, -1))
-        self.SetToolTip(wx.ToolTip(_(u"Saisissez une heure")))   
+
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+
+        try:
+            largeur = max(UTILS_UIMetrics.px(68), self.GetTextExtent("00:00")[0] + UTILS_UIMetrics.spacing(4))
+            hauteur = UTILS_UIMetrics.action_target("compact")
+            self.SetMinSize((largeur, hauteur))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
+        self.SetToolTip(wx.ToolTip(_(u"Saisissez une heure")))
         self.Bind(wx.EVT_KILL_FOCUS, self.OnKillFocus)
-    
+
     def StrEnDatetime(self, texteHeure):
         texteHeure = texteHeure[:5]
         posTemp = texteHeure.index(":")
         heuresTemp = int(texteHeure[:posTemp])
-        minutesTemp =  int(texteHeure[posTemp+1:])
-        heure = datetime.time(heuresTemp, minutesTemp)
-        return heure
-    
+        minutesTemp = int(texteHeure[posTemp + 1:])
+        return datetime.time(heuresTemp, minutesTemp)
+
     def Validation(self):
         texteBrut = self.GetPlainValue()
-        if len(texteBrut) != 4 :
+        if len(texteBrut) != 4:
             return False
-        try :
+        try:
             if texteBrut == "":
                 return False
-            # Vérifie chaque chiffre
             for chiffre in texteBrut:
                 if chiffre != " ":
-                    if not (0 <= int(chiffre) <=9):
+                    if not (0 <= int(chiffre) <= 9):
                         return False
                 else:
                     return False
-            # Vérification de l'ensemble de la date
-            if not (0<= int(texteBrut[:2]) <=self.heure_max):
+            if not (0 <= int(texteBrut[:2]) <= self.heure_max):
                 return False
-            if not (0<= int(texteBrut[-2:]) <=59):
+            if not (0 <= int(texteBrut[-2:]) <= 59):
                 return False
             return True
-        except :
+        except Exception:
             return False
 
     def SetHeure(self, heure):
-        if heure == None :
+        if heure is None:
             return
         self.SetValue(heure)
-    
+
     def GetHeure(self):
         heure = self.GetValue()
-        if heure == "  :  " :
+        if heure == "  :  ":
             return None
-        else:
-            return heure
-        
+        return heure
+
     def OnKillFocus(self, event):
         self.Validation()
         event.Skip()
@@ -81,21 +104,21 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl1 = Heure(panel)
         self.ctrl2 = Heure(panel)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl1, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(self.ctrl2, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl1, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.ctrl2, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
