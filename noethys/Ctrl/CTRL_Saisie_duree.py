db48e7a7f8272380577aabb9bc3b6a1f95c73236
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:31:06 +0200

    UI: modernise la saisie des durées

--- noethys/Ctrl/CTRL_Saisie_duree.py
+++ noethys/Ctrl/CTRL_Saisie_duree.py
@@ -8,24 +8,26 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
+import datetime
 import wx
 import six
-import datetime
+
+from Utils.UTILS_Traduction import _
 from Utils import UTILS_Dates
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+
 
 if 'phoenix' in wx.PlatformInfo:
     validator = wx.Validator
     IsSilent = wx.Validator.IsSilent
-else :
+else:
     validator = wx.PyValidator
     IsSilent = wx.Validator_IsSilent
 
 
 CARACT_AUTORISES = "0123456789-:.hH"
+
 
 class MyValidator(validator):
     def __init__(self):
@@ -38,30 +40,21 @@ class MyValidator(validator):
     def Validate(self, win):
         tc = self.GetWindow()
         val = tc.GetValue()
-
-        for x in val:
-            if x not in CARACT_AUTORISES :
+        for caractere in val:
+            if caractere not in CARACT_AUTORISES:
                 return False
-
         return True
 
     def OnChar(self, event):
         key = event.GetKeyCode()
-
         if key < wx.WXK_SPACE or key == wx.WXK_DELETE or key > 255:
             event.Skip()
             return
-
         if chr(key) in CARACT_AUTORISES:
             event.Skip()
             return
-
         if not IsSilent():
             wx.Bell()
-
-        # Returning without calling even.Skip eats the event before it
-        # gets to the text control
-        return
 
     def TransferToWindow(self):
         return True
@@ -70,70 +63,91 @@ class MyValidator(validator):
         return True
 
 
-#-----------------------------------------------------------------------------------------------------
 class CTRL(wx.TextCtrl):
+    """Saisie de durée compatible thème, DPI et grosse police."""
+
     def __init__(self, parent, separateur="h", font=None, size=(-1, -1), style=wx.TE_PROCESS_ENTER | wx.TE_CENTER):
         wx.TextCtrl.__init__(self, parent, -1, "", size=size, validator=MyValidator(), style=style)
         self.parent = parent
         self.separateur = separateur
+        self._AppliqueStyle(font)
         self.SetToolTip(wx.ToolTip(_(u"Saisissez une durée.\n\nExemples de formats acceptés :\n12h45, 6:32, 12.5, 45h, 1725H30")))
         self.Bind(wx.EVT_KILL_FOCUS, self.OnKillFocus)
         self.SetValue(datetime.timedelta(0))
         self.oldValeur = self.GetValue()
-        if font != None :
+
+    def _AppliqueStyle(self, font=None):
+        try:
+            if font is None:
+                font = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+                facteur = UTILS_Interface.GetTailleTexte() / 100.0
+                font.SetPointSize(max(8, int(round(font.GetPointSize() * facteur))))
             self.SetFont(font)
+        except Exception:
+            pass
+
+        try:
+            largeur = max(
+                UTILS_UIMetrics.px(84),
+                self.GetTextExtent("000h00")[0] + UTILS_UIMetrics.spacing(4),
+            )
+            self.SetMinSize((largeur, UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
 
     def OnKillFocus(self, event):
         valide, messageErreur = self.Validation()
-        if valide == False :
-            wx.MessageBox(messageErreur, "Erreur de saisie")
+        if valide is False:
+            dlg = wx.MessageDialog(self, messageErreur, _(u"Erreur de saisie"), wx.OK | wx.ICON_EXCLAMATION)
+            dlg.ShowModal()
+            dlg.Destroy()
             self.SetDuree(self.oldValeur)
         else:
             self.SetDuree(self.GetDuree())
             self.oldValeur = self.GetDuree()
-        if event != None : event.Skip()
+        if event is not None:
+            event.Skip()
 
     def Validation(self):
-        try :
-            valeur = self.GetDuree()
-        except :
+        try:
+            self.GetDuree()
+        except Exception:
             message = _(u"La durée que vous avez saisi ne semble pas valide.")
             return False, message
         return True, None
 
     def GetDuree(self, format=datetime.timedelta):
-        """ format = datetime.timedelta ou str ou float"""
+        """format = datetime.timedelta ou str ou float."""
         valeur = wx.TextCtrl.GetValue(self)
-        valeur = valeur.replace(' ','')
-        valeur = valeur.replace('h',':')
-        valeur = valeur.replace('H',':')
-        if valeur == ":" :
+        valeur = valeur.replace(' ', '')
+        valeur = valeur.replace('h', ':')
+        valeur = valeur.replace('H', ':')
+        if valeur == ":":
             valeur = None
 
-        # Vérifie si c'est un float
-        try :
+        try:
             valeur = float(valeur)
             format = float
-        except:
+        except Exception:
             pass
 
-        # Conversion
-        if format == datetime.timedelta :
+        if format == datetime.timedelta:
             return UTILS_Dates.HeureStrEnDelta(valeur)
-        elif format == float :
+        if format == float:
             return datetime.timedelta(hours=valeur)
-        else :
-            return valeur
+        return valeur
 
     def SetDuree(self, duree=None):
-        """ duree = float, str ou timedelta """
-        if type(duree) == float :
+        """duree = float, str ou timedelta."""
+        if type(duree) == float:
             td = datetime.timedelta(hours=duree)
         elif type(duree) in (str, six.text_type):
             td = UTILS_Dates.HeureStrEnDelta(duree)
-        elif type(duree) == datetime.timedelta :
+        elif type(duree) == datetime.timedelta:
             td = duree
-        else :
+        else:
             td = datetime.timedelta(0)
         valeur = UTILS_Dates.DeltaEnStr(td, separateur=self.separateur)
         wx.TextCtrl.SetValue(self, valeur)
@@ -145,44 +159,30 @@ class CTRL(wx.TextCtrl):
         return self.GetDuree()
 
 
-
-
-
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl = CTRL(panel)
         self.bouton = wx.Button(panel, -1, "TEST")
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(self.bouton, 0, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 0, wx.ALL, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.bouton, 0, wx.ALL, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
         self.Bind(wx.EVT_BUTTON, self.OnBouton, self.bouton)
 
     def OnBouton(self, event):
-        """ Test des formats possibles """
-        # Importation des données
-        #self.ctrl.SetDuree(datetime.timedelta(hours=123, minutes=0))
-        #self.ctrl.SetDuree("12:45")
-        #self.ctrl.SetDuree(12.5)
         self.ctrl.SetValue(13.5)
-
-        # Récupération des données
-        #print self.ctrl.GetDuree(format=datetime.timedelta)
-        #print self.ctrl.GetDuree(format=str)
-        #print self.ctrl.GetDuree(format=float)
         print(self.ctrl.GetValue())
 
 
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
