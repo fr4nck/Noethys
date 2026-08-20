8efb6e5861b84cef3d1d788b213839799126fce8
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:31:26 +0200

    UI: modernise le sélecteur de pays

--- noethys/Ctrl/CTRL_Saisie_pays.py
+++ noethys/Ctrl/CTRL_Saisie_pays.py
@@ -8,77 +8,101 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
+import sqlite3
+import wx
 
 import Chemins
-from Utils import UTILS_Adaptations
 from Utils.UTILS_Traduction import _
-import wx
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 from Ctrl import CTRL_Bouton_image
-import sys
-import sqlite3
 
 
-    
 class SaisiePays(wx.Panel):
+    """Sélection d'un pays ou d'une nationalité."""
+
     def __init__(self, parent, mode="pays"):
-        """ Selection d'un pays ou d'une nationalite """
         wx.Panel.__init__(self, parent, id=-1, style=wx.TAB_TRAVERSAL)
         self.parent = parent
-        self.mode = mode # Mode = "pays" ou "nationalite"
+        self.mode = mode
         self.IDpays = None
-        self.image_pays = wx.StaticBitmap(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/Drapeaux/france.png"), wx.BITMAP_TYPE_PNG), size=(22, 20))
-        self.bouton_pays = wx.Button(self, -1, "...", size=(20, 20))
-        
-        self.__set_properties()
+
+        self.image_pays = wx.StaticBitmap(self, -1, self._ChargeDrapeau("france"))
+        self.bouton_pays = CTRL_Bouton_image.CTRL(
+            self,
+            texte="",
+            iconeFluent="edit",
+            tailleImage=(UTILS_UIMetrics.icon_size("inline"), UTILS_UIMetrics.icon_size("inline")),
+        )
+
+        self._AppliqueStyle()
         self.__do_layout()
-        
         self.Bind(wx.EVT_BUTTON, self.OnBoutonPays, self.bouton_pays)
-        
         self.SetValue(100)
 
-    def __set_properties(self):
-        pass
+    def _ChargeDrapeau(self, code):
+        chemin = Chemins.GetStaticPath("Images/Drapeaux/%s.png" % code)
+        try:
+            image = wx.Image(chemin, wx.BITMAP_TYPE_PNG)
+            hauteur = UTILS_UIMetrics.icon_size("inline")
+            largeur = max(hauteur, int(round(hauteur * 1.15)))
+            if image.IsOk():
+                image = image.Scale(largeur, hauteur, wx.IMAGE_QUALITY_HIGH)
+                return wx.Bitmap(image)
+        except Exception:
+            pass
+        return wx.Bitmap(chemin, wx.BITMAP_TYPE_PNG)
+
+    def _AppliqueStyle(self):
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+            cible = UTILS_UIMetrics.action_target("standard")
+            self.image_pays.SetMinSize((cible, cible))
+        except Exception:
+            pass
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=1, cols=2, vgap=5, hgap=5)
-        grid_sizer_base.Add(self.image_pays, 0, 0, 0)
-        grid_sizer_base.Add(self.bouton_pays, 0, 0, 0)
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.image_pays, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, UTILS_UIMetrics.spacing(1))
+        sizer.Add(self.bouton_pays, 0, wx.ALIGN_CENTER_VERTICAL)
+        self.SetSizer(sizer)
+        self.Fit()
         self.Layout()
-        
+
     def SetValue(self, IDpays=None, nomPays=None):
-        """ Recherche par l'IDpays ou le nom du pays """
-        if IDpays == None and nomPays == None : return
-        if IDpays != None : pays = self.Recherche_Pays(IDpays=IDpays) 
-        if nomPays != None : pays = self.Recherche_Pays(nomPays=nomPays)
+        """Recherche par l'IDpays ou le nom du pays."""
+        if IDpays is None and nomPays is None:
+            return
+        if IDpays is not None:
+            pays = self.Recherche_Pays(IDpays=IDpays)
+        if nomPays is not None:
+            pays = self.Recherche_Pays(nomPays=nomPays)
         self.IDpays = pays[0]
-        self.image_pays.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/Drapeaux/" + pays[1] + ".png"), wx.BITMAP_TYPE_PNG))
-        self.image_pays.SetToolTip(wx.ToolTip(_(u"Pays de naissance : ") + pays[2]))
-        if self.mode == "pays" :
-            # Mode pays
+        self.image_pays.SetBitmap(self._ChargeDrapeau(pays[1]))
+        if self.mode == "pays":
             self.bouton_pays.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour sélectionner un autre pays de naissance")))
             self.image_pays.SetToolTip(wx.ToolTip(_(u"Pays de naissance : %s") % pays[2]))
         else:
-            # Mode nationalité
             self.image_pays.SetToolTip(wx.ToolTip(_(u"Nationalité : %s") % pays[3]))
             self.bouton_pays.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour sélectionner une autre nationalité")))
-    
+        self.Layout()
+
     def GetValue(self):
         return self.IDpays
 
     def Recherche_Pays(self, IDpays=0, nomPays=""):
-        """ Récupération de la liste des pays dans la base """
+        """Récupération de la liste des pays dans la base."""
         con = sqlite3.connect(Chemins.GetStaticPath("Databases/Geographie.dat"))
         cur = con.cursor()
-        if nomPays == "" :
+        if nomPays == "":
             req = "SELECT IDpays, code_drapeau, nom, nationalite FROM pays WHERE IDpays=%d" % IDpays
         else:
             req = "SELECT IDpays, code_drapeau, nom, nationalite FROM pays WHERE nom='%s'" % nomPays
         cur.execute(req)
         listePays = cur.fetchall()
         con.close()
-        if len(listePays) == 0 : return
+        if len(listePays) == 0:
+            return
         return listePays[0]
 
     def OnBoutonPays(self, event):
@@ -94,19 +118,19 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
-        self.ctrl= SaisiePays(panel)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        self.ctrl = SaisiePays(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 0, wx.ALL, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
