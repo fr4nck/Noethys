eeb6ab0eafd7ffaefd1679a823a920cf8b91c62d
fr4nck <f.bellardie@gmail.com>
2026-08-20 19:07:21 +0200

    UI: simplifie et fluidifie le panneau de recherche individus

--- noethys/Ctrl/CTRL_Recherche_individus.py
+++ noethys/Ctrl/CTRL_Recherche_individus.py
@@ -13,9 +13,9 @@ import Chemins
 from Utils import UTILS_Adaptations
 from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 from Ol import OL_Individus
 from Utils import UTILS_Config
+from Utils import UTILS_Interface
 
 ID_CREER_FAMILLE = wx.Window.NewControlId()
 ID_MODIFIER_FAMILLE = wx.Window.NewControlId()
@@ -28,12 +28,11 @@ ID_OUTILS = wx.Window.NewControlId()
 
 class ToolBar(wx.ToolBar):
     def __init__(self, *args, **kwds):
-        kwds["style"] = wx.TB_FLAT|wx.TB_TEXT
+        kwds["style"] = wx.TB_FLAT | wx.TB_TEXT | wx.TB_NODIVIDER
         wx.ToolBar.__init__(self, *args, **kwds)
 
-        # Boutons
         liste_boutons = [
-            {"ID" : ID_CREER_FAMILLE, "label" : _(u"Ajouter"), "image" : "Images/32x32/Famille_ajouter.png", "tooltip" : _(u"Créer une nouvelle famille")},
+            {"ID": ID_CREER_FAMILLE, "label": _(u"Ajouter"), "image": "Images/32x32/Famille_ajouter.png", "tooltip": _(u"Créer une nouvelle famille")},
             None,
             {"ID": ID_MODIFIER_FAMILLE, "label": _(u"Modifier"), "image": "Images/32x32/Famille_modifier.png", "tooltip": _(u"Modifier la fiche famille de l'individu sélectionné")},
             {"ID": ID_SUPPRIMER_FAMILLE, "label": _(u"Supprimer"), "image": "Images/32x32/Famille_supprimer.png", "tooltip": _(u"Supprimer ou détacher l'individu sélectionné")},
@@ -41,19 +40,19 @@ class ToolBar(wx.ToolBar):
             {"ID": ID_OUVRIR_GRILLE, "label": _(u"Calendrier"), "image": "Images/32x32/Calendrier.png", "tooltip": _(u"Ouvrir la grille des consommations de l'individu sélectionné\n(ou double-clic sur la ligne + touche CTRL enfoncée)")},
             {"ID": ID_OUVRIR_FICHE_IND, "label": _(u"Fiche ind."), "image": "Images/32x32/Personnes.png", "tooltip": _(u"Ouvrir la fiche individuelle de l'individu sélectionné\n(ou double-clic sur la ligne + touche SHIFT enfoncée)")},
             None,
-            {"ID": ID_PARAMETRES, "label": _(u"Paramètres"), "image": "Images/32x32/Configuration2.png", "type": wx.ITEM_NORMAL, "tooltip": _(u"Sélectionner les paramètres d'affichage")},
+            {"ID": ID_PARAMETRES, "label": _(u"Paramètres"), "image": "Images/32x32/Configuration2.png", "tooltip": _(u"Sélectionner les paramètres d'affichage")},
             {"ID": ID_OUTILS, "label": _(u"Outils"), "image": "Images/32x32/Configuration.png", "tooltip": _(u"Outils")},
-            ]
-        for bouton in liste_boutons :
-            if bouton == None :
+        ]
+        for bouton in liste_boutons:
+            if bouton is None:
                 self.AddSeparator()
-            else :
-                try :
-                    self.AddTool(bouton["ID"], bouton["label"], wx.Bitmap(Chemins.GetStaticPath(bouton["image"]), wx.BITMAP_TYPE_ANY), wx.NullBitmap, wx.ITEM_NORMAL, bouton["tooltip"], "")
-                except :
-                    self.AddLabelTool(bouton["ID"], bouton["label"], wx.Bitmap(Chemins.GetStaticPath(bouton["image"]), wx.BITMAP_TYPE_ANY), wx.NullBitmap, wx.ITEM_NORMAL, bouton["tooltip"], "")
+            else:
+                bitmap = wx.Bitmap(Chemins.GetStaticPath(bouton["image"]), wx.BITMAP_TYPE_ANY)
+                try:
+                    self.AddTool(bouton["ID"], bouton["label"], bitmap, wx.NullBitmap, wx.ITEM_NORMAL, bouton["tooltip"], "")
+                except Exception:
+                    self.AddLabelTool(bouton["ID"], bouton["label"], bitmap, wx.NullBitmap, wx.ITEM_NORMAL, bouton["tooltip"], "")
 
-        # Binds
         self.Bind(wx.EVT_TOOL, self.Ajouter_famille, id=ID_CREER_FAMILLE)
         self.Bind(wx.EVT_TOOL, self.Modifier_famille, id=ID_MODIFIER_FAMILLE)
         self.Bind(wx.EVT_TOOL, self.Supprimer_famille, id=ID_SUPPRIMER_FAMILLE)
@@ -61,10 +60,14 @@ class ToolBar(wx.ToolBar):
         self.Bind(wx.EVT_TOOL, self.Ouvrir_fiche_ind, id=ID_OUVRIR_FICHE_IND)
         self.Bind(wx.EVT_TOOL, self.Parametres, id=ID_PARAMETRES)
         self.Bind(wx.EVT_TOOL, self.MenuOutils, id=ID_OUTILS)
-        
+
+        # Les bitmaps restent denses (desktop) mais vivent dans une barre native
+        # suffisamment haute pour constituer une vraie cible d'action.
         self.SetToolBitmapSize((32, 32))
+        hauteur_cible = int(round(40 * max(1.0, UTILS_Interface.GetEchelle() / 100.0)))
+        self.SetMinSize((-1, max(40, min(56, hauteur_cible))))
         self.Realize()
-    
+
     def Ajouter_famille(self, event):
         self.GetParent().ctrl_listview.Ajouter(None)
 
@@ -89,12 +92,10 @@ class ToolBar(wx.ToolBar):
             UTILS_Config.SetParametre("liste_individus_parametres", dlg.GetParametres())
         dlg.Destroy()
 
-        # Actualise la liste d'individus
-        self.GetParent().ActualiseParametresAffichage() 
-        self.GetParent().ctrl_listview.MAJ(forceActualisation=True)
-        
+        self.GetParent().ActualiseParametresAffichage()
+        self.GetParent().MAJ()
+
     def MenuOutils(self, event):
-        # Création du menu Outils
         menuPop = UTILS_Adaptations.Menu()
 
         ID_ACTUALISER = wx.Window.NewControlId()
@@ -108,24 +109,24 @@ class ToolBar(wx.ToolBar):
         item.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Apercu.png"), wx.BITMAP_TYPE_PNG))
         menuPop.AppendItem(item)
         self.Bind(wx.EVT_MENU, self.Apercu, id=ID_APERCU)
-        
+
         item = wx.MenuItem(menuPop, ID_IMPRIMER, _(u"Imprimer"), _(u"Imprimer la liste des effectifs affichée"))
         item.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Imprimante.png"), wx.BITMAP_TYPE_PNG))
         menuPop.AppendItem(item)
         self.Bind(wx.EVT_MENU, self.Imprimer, id=ID_IMPRIMER)
-        
+
         menuPop.AppendSeparator()
 
         item = wx.MenuItem(menuPop, ID_EXPORT_TEXTE, _(u"Exporter au format Texte"), _(u"Exporter au format Texte"))
         item.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Texte2.png"), wx.BITMAP_TYPE_PNG))
         menuPop.AppendItem(item)
         self.Bind(wx.EVT_MENU, self.GetParent().ctrl_listview.ExportTexte, id=ID_EXPORT_TEXTE)
-        
+
         item = wx.MenuItem(menuPop, ID_EXPORT_EXCEL, _(u"Exporter au format Excel"), _(u"Exporter au format Excel"))
         item.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Excel.png"), wx.BITMAP_TYPE_PNG))
         menuPop.AppendItem(item)
         self.Bind(wx.EVT_MENU, self.GetParent().ctrl_listview.ExportExcel, id=ID_EXPORT_EXCEL)
-        
+
         menuPop.AppendSeparator()
 
         item = wx.MenuItem(menuPop, ID_ACTUALISER, _(u"Actualiser"), _(u"Actualiser l'affichage"))
@@ -134,7 +135,7 @@ class ToolBar(wx.ToolBar):
         self.Bind(wx.EVT_MENU, self.Actualiser, id=ID_ACTUALISER)
 
         menuPop.AppendSeparator()
-        
+
         item = wx.MenuItem(menuPop, ID_AIDE, _(u"Aide"), _(u"Aide"))
         item.SetBitmap(wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Aide.png"), wx.BITMAP_TYPE_PNG))
         menuPop.AppendItem(item)
@@ -159,37 +160,54 @@ class ToolBar(wx.ToolBar):
 class Panel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, name="recherche_individus", id=-1, style=wx.TAB_TRAVERSAL)
-        
+
         self.toolBar = ToolBar(self)
-        self.ctrl_listview = OL_Individus.ListView(self, id=-1, style=wx.LC_REPORT|wx.SUNKEN_BORDER|wx.LC_SINGLE_SEL|wx.LC_HRULES|wx.LC_VRULES)
+        self.ctrl_listview = OL_Individus.ListView(
+            self,
+            id=-1,
+            style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES | wx.LC_VRULES,
+        )
         self.ctrl_recherche = OL_Individus.BarreRecherche(self, historique=True)
-        
+
         self.__set_properties()
         self.__do_layout()
-        
-        # Recherche paramètres d'affichage de la liste des individus
+
         self.ActualiseParametresAffichage()
 
     def __set_properties(self):
-        pass
+        self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=3, cols=1, vgap=0, hgap=0)
-        grid_sizer_base.Add(self.toolBar, 1, wx.EXPAND|wx.ALL, 0)
-        grid_sizer_base.Add(self.ctrl_listview, 1, wx.EXPAND|wx.ALL, 0)
-        grid_sizer_base.Add(self.ctrl_recherche, 1, wx.EXPAND|wx.ALL, 0)
-        grid_sizer_base.AddGrowableRow(1)
-        grid_sizer_base.AddGrowableCol(0)
-        self.SetSizer(grid_sizer_base)
+        # Le vieux FlexGridSizer n'apportait rien ici : trois éléments verticaux,
+        # dont la liste doit absorber tout l'espace disponible.
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.toolBar, 0, wx.EXPAND)
+        sizer.Add(self.ctrl_listview, 1, wx.EXPAND)
+        sizer.Add(self.ctrl_recherche, 0, wx.EXPAND)
+        self.SetSizer(sizer)
         self.Layout()
-        
+
+    def _AppliqueListeModerne(self):
+        """Retire la colonne d'avatars décoratifs du panneau d'accueil.
+
+        La suppression du chargement des images est effectuée ensuite dans
+        ``OL_Individus`` ; ici on garantit déjà qu'aucune largeur écran n'est
+        gaspillée par cette colonne historique.
+        """
+        try:
+            if self.ctrl_listview.GetColumnCount() > 0:
+                self.ctrl_listview.SetColumnWidth(0, 0)
+        except Exception:
+            pass
+
     def MAJ(self):
         self.ctrl_listview.MAJ(forceActualisation=True)
-        
+        self._AppliqueListeModerne()
+
     def Aide(self):
         from Utils import UTILS_Aide
         UTILS_Aide.Aide("Lalistedesindividus")
-    
+
     def ActualiseParametresAffichage(self):
         parametres = UTILS_Config.GetParametre("liste_individus_parametres", defaut="")
         self.ctrl_listview.SetParametres(parametres)
@@ -200,20 +218,20 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
         sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
+        sizer_1.Add(panel, 1, wx.ALL | wx.EXPAND)
         self.SetSizer(sizer_1)
-        self.ctrl= Panel(panel)
-        self.ctrl.MAJ() 
+        self.ctrl = Panel(panel)
+        self.ctrl.MAJ()
         sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
+        sizer_2.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, 4)
         panel.SetSizer(sizer_2)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
-    frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
+    frame_1 = MyFrame(None, -1, "TEST", size=(1000, 600))
     app.SetTopWindow(frame_1)
     frame_1.Show()
     app.MainLoop()
