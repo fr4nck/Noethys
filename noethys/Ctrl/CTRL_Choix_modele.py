d4747392f7b8e21788fb0c56af82dc6c5ce01416
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:34:31 +0200

    UI: modernise le choix des modèles de documents

--- noethys/Ctrl/CTRL_Choix_modele.py
+++ noethys/Ctrl/CTRL_Choix_modele.py
@@ -8,45 +8,56 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 import GestionDB
 
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
 
 
 class CTRL_Choice(wx.Choice):
+    """Choix d'un modèle de document avec métriques desktop communes."""
+
     def __init__(self, parent, categorie=""):
-        wx.Choice.__init__(self, parent, -1) 
+        wx.Choice.__init__(self, parent, -1)
         self.parent = parent
         self.categorie = categorie
         self.defaut = None
-        self.MAJ() 
-    
+        self._AppliqueStyle()
+        self.MAJ()
+
+    def _AppliqueStyle(self):
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+        try:
+            self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
     def SetCategorie(self, categorie=""):
         self.categorie = categorie
         self.defaut = None
-        self.MAJ() 
+        self.MAJ()
         self.SetID(self.defaut)
-    
+
     def MAJ(self):
         selectionActuelle = self.GetID()
         listeItems = self.GetListeDonnees()
-        if len(listeItems) == 0 :
-            self.Enable(False)
-        else:
-            self.Enable(True)
+        self.Enable(bool(listeItems))
         self.SetItems(listeItems)
-        # Re-sélection après MAJ
-        if selectionActuelle != None :
+        if selectionActuelle is not None:
             self.SetID(selectionActuelle)
         else:
-            # Sélection par défaut
             self.SetID(self.defaut)
-                                        
+
     def GetListeDonnees(self):
         listeItems = []
         self.dictDonnees = {}
@@ -58,27 +69,24 @@ class CTRL_Choice(wx.Choice):
         DB.ExecuterReq(req)
         listeDonnees = DB.ResultatReq()
         DB.Close()
-        index = 0
-        for IDmodele, nom, largeur, hauteur, observations, defaut in listeDonnees :
+        for index, (IDmodele, nom, largeur, hauteur, observations, defaut) in enumerate(listeDonnees):
             listeItems.append(nom)
-            self.dictDonnees[index] = {"ID" : IDmodele}
-            if defaut == 1 :
+            self.dictDonnees[index] = {"ID": IDmodele}
+            if defaut == 1:
                 self.defaut = IDmodele
-            index += 1
         return listeItems
 
     def SetID(self, ID=None):
         for index, values in self.dictDonnees.items():
-            if values != None and values["ID"] == ID :
-                 self.SetSelection(index)
+            if values is not None and values["ID"] == ID:
+                self.SetSelection(index)
 
     def GetID(self):
         index = self.GetSelection()
-        if index == -1 : return None
+        if index == -1:
+            return None
         return self.dictDonnees[index]["ID"]
-            
-# -------------------------------------------------------------------------------------------------------------------------------------------
-    
+
 
 def DemandeModele(categorie=""):
     IDmodele = None
@@ -92,15 +100,13 @@ def DemandeModele(categorie=""):
     DB.Close()
     listeLabels = []
     indexDefaut = None
-    index = 0
-    for IDmodele, nom, defaut in listeDonnees :
+    for index, (IDmodele, nom, defaut) in enumerate(listeDonnees):
         listeLabels.append(nom)
-        if defaut == 1 :
+        if defaut == 1:
             indexDefaut = index
-        index += 1
     dlg = wx.SingleChoiceDialog(None, _(u"Veuillez sélectionner un modèle dans la liste :"), _(u"Sélection d'un modèle"), listeLabels, wx.CHOICEDLG_STYLE)
-    if indexDefaut != None :
-        dlg.SetSelection(indexDefaut)        
+    if indexDefaut is not None:
+        dlg.SetSelection(indexDefaut)
     if dlg.ShowModal() == wx.ID_OK:
         selection = dlg.GetSelection()
         IDmodele = listeDonnees[selection][0]
@@ -114,27 +120,23 @@ def DemandeModele(categorie=""):
     return IDmodele
 
 
-
-
-
-
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl = CTRL_Choice(panel, categorie="facture")
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
