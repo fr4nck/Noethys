14c7dff3ffffa0b5a519801f3bb678129bee1a40
fr4nck <f.bellardie@gmail.com>
2026-08-20 21:32:36 +0200

    UI: modernise le choix des relevés bancaires

--- noethys/Ctrl/CTRL_Saisie_releve_bancaire.py
+++ noethys/Ctrl/CTRL_Saisie_releve_bancaire.py
@@ -8,32 +8,47 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 import GestionDB
 
-    
+from Utils.UTILS_Traduction import _
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+from Ctrl import CTRL_Bouton_image
+
+
 class CTRL_Choix(wx.Choice):
     def __init__(self, parent, IDcompte_bancaire=None):
-        wx.Choice.__init__(self, parent, -1) 
+        wx.Choice.__init__(self, parent, -1)
         self.parent = parent
         self.IDcompte_bancaire = IDcompte_bancaire
-        self.MAJ() 
-    
+        self._AppliqueStyle()
+        self.MAJ()
+
+    def _AppliqueStyle(self):
+        try:
+            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+            self.SetFont(police)
+        except Exception:
+            pass
+        try:
+            self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
     def MAJ(self):
         listeItems = self.GetListeDonnees()
-        if len(listeItems) == 0 :
-            self.Enable(False)
+        self.Enable(bool(listeItems) and self.IDcompte_bancaire is not None)
         self.SetItems(listeItems)
-    
+
     def GetListeDonnees(self):
-        listeItems = [u"",]
-        self.dictDonnees = { 0 : {"ID":None}, }
-        if self.IDcompte_bancaire == None :
+        listeItems = [u""]
+        self.dictDonnees = {0: {"ID": None}}
+        if self.IDcompte_bancaire is None:
             return listeItems
         DB = GestionDB.DB()
         req = """SELECT IDreleve, nom, date_debut, date_fin
@@ -43,51 +58,55 @@ class CTRL_Choix(wx.Choice):
         DB.ExecuterReq(req)
         listeDonnees = DB.ResultatReq()
         DB.Close()
-        index = 1
-        for IDreleve, nom, date_debut, date_fin in listeDonnees :
-            self.dictDonnees[index] = { "ID" : IDreleve }
-            label = nom
-            listeItems.append(label)
-            index += 1
+        for index, (IDreleve, nom, date_debut, date_fin) in enumerate(listeDonnees, start=1):
+            self.dictDonnees[index] = {"ID": IDreleve}
+            listeItems.append(nom)
         return listeItems
 
     def SetID(self, ID=0):
         for index, values in self.dictDonnees.items():
-            if values["ID"] == ID :
-                 self.SetSelection(index)
+            if values["ID"] == ID:
+                self.SetSelection(index)
 
     def GetID(self):
         index = self.GetSelection()
-        if index == -1 : return None
+        if index == -1:
+            return None
         return self.dictDonnees[index]["ID"]
 
-
-
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class CTRL(wx.Panel):
     def __init__(self, parent, IDcompte_bancaire=None, afficherBouton=True):
         wx.Panel.__init__(self, parent, id=-1, name="ctrl_saisie_releve_bancaire", style=wx.TAB_TRAVERSAL)
         self.parent = parent
         self.afficherBouton = afficherBouton
-        
         self.ctrl_releve = CTRL_Choix(self, IDcompte_bancaire=IDcompte_bancaire)
-        
-        if self.afficherBouton == True :
-            self.bouton_releve = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath(u"Images/16x16/Mecanisme.png"), wx.BITMAP_TYPE_ANY))
+
+        self.bouton_releve = None
+        if self.afficherBouton is True:
+            self.bouton_releve = CTRL_Bouton_image.CTRL(
+                self,
+                texte="",
+                iconeFluent="edit",
+                tailleImage=(UTILS_UIMetrics.icon_size("inline"), UTILS_UIMetrics.icon_size("inline")),
+            )
             self.Bind(wx.EVT_BUTTON, self.OnBoutonReleve, self.bouton_releve)
             self.bouton_releve.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour accéder à la gestion des relevés bancaires")))
 
-        grid_sizer_base = wx.FlexGridSizer(rows=1, cols=2, vgap=5, hgap=5)
-        grid_sizer_base.Add(self.ctrl_releve, 1, wx.EXPAND|wx.ALL, 0)
-        if self.afficherBouton == True :
-            grid_sizer_base.Add(self.bouton_releve, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 0)
-        grid_sizer_base.AddGrowableCol(0)
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+        except Exception:
+            pass
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.ctrl_releve, 1, wx.EXPAND)
+        if self.bouton_releve is not None:
+            sizer.Add(self.bouton_releve, 0, wx.LEFT | wx.ALIGN_CENTER_VERTICAL, UTILS_UIMetrics.spacing(1))
+        self.SetSizer(sizer)
+        self.Fit()
         self.Layout()
 
-    def OnBoutonReleve(self, event):  
+    def OnBoutonReleve(self, event):
         IDreleve = self.ctrl_releve.GetID()
         from Dlg import DLG_Releves_compta
         dlg = DLG_Releves_compta.Dialog(self)
@@ -95,7 +114,7 @@ class CTRL(wx.Panel):
         dlg.Destroy()
         self.ctrl_releve.MAJ()
         self.ctrl_releve.SetID(IDreleve)
-        
+
     def SetIDcompte_bancaire(self, IDcompte_bancaire=None):
         self.ctrl_releve.IDcompte_bancaire = IDcompte_bancaire
 
@@ -104,33 +123,32 @@ class CTRL(wx.Panel):
 
     def GetID(self):
         return self.ctrl_releve.GetID()
-    
+
     def MAJ(self):
         IDreleve = self.ctrl_releve.GetID()
         self.ctrl_releve.MAJ()
         self.ctrl_releve.SetID(IDreleve)
-        
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1, name="panel_test")
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        sizer_1.Add(panel, 1, wx.ALL|wx.EXPAND)
-        self.SetSizer(sizer_1)
         self.ctrl1 = CTRL(panel)
         self.ctrl2 = CTRL(panel, afficherBouton=False)
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)
-        sizer_2.Add(self.ctrl1, 1, wx.ALL|wx.EXPAND, 4)
-        sizer_2.Add(self.ctrl2, 1, wx.ALL|wx.EXPAND, 4)
-        panel.SetSizer(sizer_2)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.ctrl1, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.ctrl2, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        panel.SetSizer(sizer)
+        cadre = wx.BoxSizer(wx.VERTICAL)
+        cadre.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(cadre)
         self.Layout()
         self.CentreOnScreen()
 
+
 if __name__ == '__main__':
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     frame_1 = MyFrame(None, -1, "TEST", size=(800, 400))
     app.SetTopWindow(frame_1)
     frame_1.Show()
