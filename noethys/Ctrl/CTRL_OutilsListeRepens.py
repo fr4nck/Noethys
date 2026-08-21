9e4d5af5b61a616eaee2da284a8c1cf55f139749
fr4nck <f.bellardie@gmail.com>
2026-08-21 07:25:30 +0200

    UI: complète la barre de liste Repens avec l'API historique

--- noethys/Ctrl/CTRL_OutilsListeRepens.py
+++ noethys/Ctrl/CTRL_OutilsListeRepens.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Recherche et filtres pour les ObjectListView modernisées.
+"""Barre d'outils Repens pour les ObjectListView Noethys.
 
-Remplace progressivement ``CTRL_ObjectListView.CTRL_Outils`` dans les écrans
-Repens sans modifier le composant historique utilisé ailleurs.
+Cette implémentation reprend volontairement le contrat public de
+``CTRL_ObjectListView.CTRL_Outils`` afin de pouvoir migrer les écrans un par un
+sans modifier leur logique métier : recherche, filtres avancés, cochage rapide
+et regroupement restent disponibles, mais sans ``PlateButton`` ni images 16 px.
 """
 
 import wx
 
 from Ctrl import CTRL_ActionRepens
+from Utils import UTILS_Adaptations
 from Utils import UTILS_Interface
 from Utils import UTILS_UIMetrics
 from Utils.UTILS_Traduction import _
 
 
+ID_FILTRES_GERER = 10
+ID_FILTRES_EFFACER = 11
+ID_COCHER_TOUT = 20
+ID_DECOCHER_TOUT = 21
+
+
 class BarreRecherche(wx.SearchCtrl):
+    """Recherche différée, compatible avec le contrat historique."""
+
     def __init__(self, parent, listview, texteDefaut=u"Rechercher…"):
         wx.SearchCtrl.__init__(self, parent, style=wx.TE_PROCESS_ENTER)
+        self.parent = parent
         self.listview = listview
         self.SetDescriptiveText(texteDefaut)
         self.ShowSearchButton(True)
         self.ShowCancelButton(False)
         self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
         try:
-            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_low"))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
             self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
         except Exception:
             pass
@@ -38,7 +50,7 @@ class BarreRecherche(wx.SearchCtrl):
         self.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN, self.OnSearch)
         self.Bind(wx.EVT_SEARCHCTRL_CANCEL_BTN, self.OnCancel)
         self.Bind(wx.EVT_TEXT_ENTER, self.OnSearch)
-        self.Bind(wx.EVT_TEXT, self.OnTexte)
+        self.Bind(wx.EVT_TEXT, self.OnDoSearch)
         self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
 
     def OnSearch(self, event=None):
@@ -57,7 +69,7 @@ class BarreRecherche(wx.SearchCtrl):
             return
         event.Skip()
 
-    def OnTexte(self, event):
+    def OnDoSearch(self, event):
         if self.timer.IsRunning():
             self.timer.Stop()
         try:
@@ -75,6 +87,9 @@ class BarreRecherche(wx.SearchCtrl):
         self.timer.Start(duree, oneShot=True)
         event.Skip()
 
+    # Alias conservé pour les quelques appels introduits pendant la migration.
+    OnTexte = OnDoSearch
+
     def Recherche(self, event=None):
         if self.timer.IsRunning():
             self.timer.Stop()
@@ -86,10 +101,104 @@ class BarreRecherche(wx.SearchCtrl):
             self.listview.Filtrer()
 
 
+class CTRL_Regroupement(wx.Choice):
+    """Choix de regroupement compatible avec celui de CTRL_ObjectListView."""
+
+    def __init__(self, parent):
+        wx.Choice.__init__(self, parent, -1)
+        self.parent = parent
+        self.listview = None
+        self.listeLabels = []
+        self.dictDonnees = {}
+        self.SetMinSize((UTILS_UIMetrics.px(130), UTILS_UIMetrics.action_target("compact")))
+        try:
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
+        except Exception:
+            pass
+
+    def MAJ(self, listview=None):
+        if listview is not None:
+            self.listview = listview
+        if self.listview is None:
+            return
+        if not getattr(self, "_repens_bind_regroupement", False):
+            self._repens_bind_regroupement = True
+            self.Bind(wx.EVT_CHOICE, self.OnChoix)
+
+        self.dictDonnees = {0: None}
+        self.listeLabels = [_(u"Aucun")]
+        for index_colonne, titre in enumerate(self.GetTitresColonnes(self.listview)):
+            if titre not in ("ID", ""):
+                self.dictDonnees[len(self.listeLabels)] = index_colonne
+                self.listeLabels.append(titre)
+        self.SetItems(self.listeLabels)
+
+        selection = 0
+        regroupement = getattr(self.listview, "regroupement", None)
+        if regroupement is not None:
+            for index, index_colonne in self.dictDonnees.items():
+                if index_colonne == regroupement:
+                    selection = index
+                    break
+        self.SetSelection(selection)
+
+    def GetTitresColonnes(self, listview=None):
+        listview = listview or self.listview
+        if listview is None:
+            return []
+        titres = []
+        try:
+            colonnes = listview.columns
+        except Exception:
+            colonnes = []
+        for colonne in colonnes:
+            titre = getattr(colonne, "title", "")
+            titres.append(titre)
+        if titres:
+            return titres
+        try:
+            for index in range(listview.GetColumnCount()):
+                titres.append(listview.GetColumn(index).GetText())
+        except Exception:
+            pass
+        return titres
+
+    def GetRegroupement(self):
+        index = self.GetSelection()
+        if index in (-1, 0):
+            return None
+        return self.dictDonnees.get(index)
+
+    def OnChoix(self, event=None):
+        if self.listview is None:
+            return
+        self.listview.regroupement = self.GetRegroupement()
+        try:
+            self.listview.MAJ()
+        except Exception:
+            try:
+                self.listview.RepopulateList()
+            except Exception:
+                pass
+
+
 class CTRL(wx.Panel):
-    def __init__(self, parent, listview, texteDefaut=u"Rechercher…"):
-        wx.Panel.__init__(self, parent, style=wx.BORDER_NONE | wx.TAB_TRAVERSAL)
+    """Recherche, filtrage, cochage et regroupement dans une barre desktop."""
+
+    def __init__(
+        self,
+        parent,
+        listview=None,
+        texteDefaut=u"Rechercher…",
+        afficherCocher=False,
+        afficherRegroupement=False,
+        style=wx.NO_BORDER | wx.TAB_TRAVERSAL,
+    ):
+        wx.Panel.__init__(self, parent, id=-1, style=style)
         self.listview = listview
+        self.afficherRegroupement = bool(afficherRegroupement)
+        self.afficherCocher = bool(afficherCocher)
         self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
 
         self.barreRecherche = BarreRecherche(self, listview=listview, texteDefaut=texteDefaut)
@@ -102,14 +211,65 @@ class CTRL(wx.Panel):
         )
         self.Bind(wx.EVT_BUTTON, self.OnBoutonFiltrer, self.bouton_filtrer)
 
+        self.bouton_cocher = None
+        if self.afficherCocher:
+            self.bouton_cocher = CTRL_ActionRepens.CTRL(
+                self,
+                label=_(u"Cocher"),
+                variante="ghost",
+                tooltip=_(u"Cocher ou décocher rapidement les éléments de la liste"),
+            )
+            self.Bind(wx.EVT_BUTTON, self.OnBoutonCocher, self.bouton_cocher)
+
+        self.label_regroupement = None
+        self.ctrl_regroupement = None
+        if self.afficherRegroupement:
+            self.label_regroupement = wx.StaticText(self, -1, _(u"Regrouper :"))
+            self.label_regroupement.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface_variant"))
+            self.ctrl_regroupement = CTRL_Regroupement(self)
+            self.ctrl_regroupement.MAJ(listview=listview)
+            try:
+                listview.ctrl_regroupement = self.ctrl_regroupement
+            except Exception:
+                pass
+
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self.barreRecherche, 1, wx.EXPAND | wx.RIGHT, UTILS_UIMetrics.spacing(1))
         sizer.Add(self.bouton_filtrer, 0, wx.ALIGN_CENTER_VERTICAL)
+        if self.bouton_cocher is not None:
+            sizer.Add(self.bouton_cocher, 0, wx.LEFT | wx.ALIGN_CENTER_VERTICAL, UTILS_UIMetrics.spacing(1))
+        if self.ctrl_regroupement is not None:
+            sizer.AddSpacer(UTILS_UIMetrics.spacing(2))
+            sizer.Add(self.label_regroupement, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, UTILS_UIMetrics.spacing(1))
+            sizer.Add(self.ctrl_regroupement, 0, wx.ALIGN_CENTER_VERTICAL)
         self.SetSizer(sizer)
         self.Layout()
-        self.MAJFiltre()
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.MAJ_ctrl_filtrer()
+
+    def OnSize(self, event):
+        event.Skip()
+
+    def _MenuFiltres(self):
+        menu = UTILS_Adaptations.Menu()
+        menu.Append(ID_FILTRES_GERER, _(u"Gérer les filtres…"))
+        menu.Append(ID_FILTRES_EFFACER, _(u"Supprimer tous les filtres"))
+        self.Bind(wx.EVT_MENU, self.OnMenu, id=ID_FILTRES_GERER)
+        self.Bind(wx.EVT_MENU, self.OnMenu, id=ID_FILTRES_EFFACER)
+        return menu
+
+    def _MenuCocher(self):
+        menu = UTILS_Adaptations.Menu()
+        menu.Append(ID_COCHER_TOUT, _(u"Tout cocher"))
+        menu.Append(ID_DECOCHER_TOUT, _(u"Tout décocher"))
+        self.Bind(wx.EVT_MENU, self.OnMenu, id=ID_COCHER_TOUT)
+        self.Bind(wx.EVT_MENU, self.OnMenu, id=ID_DECOCHER_TOUT)
+        return menu
 
     def MAJFiltre(self):
+        self.MAJ_ctrl_filtrer()
+
+    def MAJ_ctrl_filtrer(self):
         try:
             nbre = len(self.listview.listeFiltresColonnes)
         except Exception:
@@ -117,21 +277,58 @@ class CTRL(wx.Panel):
         label = _(u"Filtrer") if nbre == 0 else _(u"Filtrer (%d)") % nbre
         self.bouton_filtrer.SetLabel(label)
         if nbre:
-            self.bouton_filtrer.SetToolTip(wx.ToolTip(_(u"%d filtre(s) avancé(s) actif(s)") % nbre))
+            texte = _(u"%d filtre(s) avancé(s) actif(s)") % nbre
         else:
-            self.bouton_filtrer.SetToolTip(wx.ToolTip(_(u"Gérer les filtres avancés de cette liste")))
+            texte = _(u"Gérer les filtres avancés de cette liste")
+        self.bouton_filtrer.SetToolTip(wx.ToolTip(texte))
+        self.Layout()
 
     def OnBoutonFiltrer(self, event=None):
+        menu = self._MenuFiltres()
+        try:
+            self.PopupMenu(menu, self.bouton_filtrer.GetPosition() + wx.Point(0, self.bouton_filtrer.GetSize().GetHeight()))
+        except Exception:
+            self.PopupMenu(menu)
+        menu.Destroy()
+
+    def OnBoutonCocher(self, event=None):
+        if self.bouton_cocher is None:
+            return
+        menu = self._MenuCocher()
+        try:
+            self.PopupMenu(menu, self.bouton_cocher.GetPosition() + wx.Point(0, self.bouton_cocher.GetSize().GetHeight()))
+        except Exception:
+            self.PopupMenu(menu)
+        menu.Destroy()
+
+    def _GererFiltres(self):
         from Dlg import DLG_Filtres_listes
         dlg = DLG_Filtres_listes.Dialog(self, ctrl_listview=self.listview)
         if dlg.ShowModal() == wx.ID_OK:
             listeFiltres = dlg.GetDonnees()
             self.listview.SetFiltresColonnes(listeFiltres)
             self.listview.Filtrer()
-            self.MAJFiltre()
+            self.MAJ_ctrl_filtrer()
         dlg.Destroy()
+
+    def OnMenu(self, event):
+        identifiant = event.GetId()
+        if identifiant == ID_FILTRES_GERER:
+            self._GererFiltres()
+        elif identifiant == ID_FILTRES_EFFACER:
+            self.SetFiltres([])
+        elif identifiant == ID_COCHER_TOUT:
+            try:
+                self.listview.CocheListeTout()
+            except Exception:
+                pass
+        elif identifiant == ID_DECOCHER_TOUT:
+            try:
+                self.listview.CocheListeRien()
+            except Exception:
+                pass
 
     def SetFiltres(self, listeFiltres=None):
         self.listview.SetFiltresColonnes(listeFiltres or [])
         self.listview.Filtrer()
-        self.MAJFiltre()
+        self.MAJ_ctrl_filtrer()
