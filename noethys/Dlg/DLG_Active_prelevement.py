effa48f919ee2ed6aef04b6fae5d4b2170b2b99b
fr4nck <f.bellardie@gmail.com>
2026-08-21 07:41:14 +0200

    ui: migrate automatic debit dialog to Repens shell

--- noethys/Dlg/DLG_Active_prelevement.py
+++ noethys/Dlg/DLG_Active_prelevement.py
@@ -5,126 +5,153 @@
 # Site internet :  www.noethys.com
 # Auteur:           Ivan LUCAS
 # Copyright:       (c) 2010-12 Ivan LUCAS
-# Licence:         Licence GNU GPL
+# Licence:          Licence GNU GPL
 #------------------------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
-from Ctrl import CTRL_Bandeau
+
 import GestionDB
+from Ctrl import CTRL_ActionRepens
+from Ctrl import CTRL_FenetreRepens
 from Ol import OL_Mandats
+from Utils import UTILS_StyleRepens as Style
+from Utils.UTILS_Traduction import _
 
 
-class Dialog(wx.Dialog):
+class Dialog(CTRL_FenetreRepens.Dialog):
+    """Activation du prélèvement, première fenêtre métier sur le shell Repens."""
+
     def __init__(self, parent, IDfamille=None):
-        wx.Dialog.__init__(self, parent, -1, style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MAXIMIZE_BOX|wx.MINIMIZE_BOX)
         self.parent = parent
         self.IDfamille = IDfamille
-                
-        # Bandeau
-        intro = _(u"Saisissez ici les coordonnées bancaires du compte de la famille à débiter afin d'activer le prélèvement automatique.")
+
+        intro = _(u"Saisissez les coordonnées bancaires du compte de la famille à débiter afin d'activer le prélèvement automatique.")
         titre = _(u"Prélèvement automatique")
-        self.SetTitle(titre)
-        self.ctrl_bandeau = CTRL_Bandeau.Bandeau(self, titre=titre, texte=intro, hauteurHtml=30, nomImage="Images/32x32/Prelevement.png")
-        
-        # Activation
-        self.box_activation_staticbox = wx.StaticBox(self, -1, _(u"Activation"))
-        self.label_activation = wx.StaticText(self, -1, _(u"Prélèvement activé :"))
-        self.radio_activation_oui = wx.RadioButton(self, -1, _(u"Oui"), style=wx.RB_GROUP)
-        self.radio_activation_non = wx.RadioButton(self, -1, _(u"Non"))
-        self.radio_activation_non.SetValue(True) 
+        CTRL_FenetreRepens.Dialog.__init__(
+            self,
+            parent,
+            titre=titre,
+            intro=intro,
+            nomImage="Images/32x32/Prelevement.png",
+            taille=(760, 560),
+            taille_min=(620, 440),
+        )
 
-        # Mandats
-        self.box_mandats_staticbox = wx.StaticBox(self, -1, _(u"Mandats SEPA"))
-        self.ctrl_listview = OL_Mandats.ListView(self, id=-1, IDfamille=self.IDfamille, style=wx.LC_REPORT|wx.SUNKEN_BORDER|wx.LC_SINGLE_SEL|wx.LC_HRULES|wx.LC_VRULES)
-        self.ctrl_listview.SetMinSize((20, 20))
+        # Activation -------------------------------------------------------
+        self.section_activation = self.AjouterSection(
+            _(u"Activation"),
+            _(u"Activez ou désactivez le prélèvement pour cette famille."),
+            proportion=0,
+        )
+        parent_activation = self.section_activation.GetContenu()
+        self.label_activation = wx.StaticText(parent_activation, -1, _(u"Prélèvement activé"))
+        Style.appliquer_texte(
+            self.label_activation,
+            role="body_emphasis",
+            role_texte="on_surface",
+            role_fond="surface_container_low",
+        )
+        self.radio_activation_oui = wx.RadioButton(parent_activation, -1, _(u"Oui"), style=wx.RB_GROUP)
+        self.radio_activation_non = wx.RadioButton(parent_activation, -1, _(u"Non"))
+        self.radio_activation_non.SetValue(True)
+        for ctrl in (self.radio_activation_oui, self.radio_activation_non):
+            Style.appliquer_texte(
+                ctrl,
+                role="body",
+                role_texte="on_surface",
+                role_fond="surface_container_low",
+            )
+
+        ligne_activation = wx.BoxSizer(wx.HORIZONTAL)
+        ligne_activation.Add(self.label_activation, 0, wx.ALIGN_CENTER_VERTICAL)
+        ligne_activation.AddStretchSpacer(1)
+        ligne_activation.Add(self.radio_activation_oui, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, Style.espace(2))
+        ligne_activation.Add(self.radio_activation_non, 0, wx.ALIGN_CENTER_VERTICAL)
+        self.section_activation.GetSizerContenu().Add(ligne_activation, 0, wx.EXPAND)
+
+        # Mandats ----------------------------------------------------------
+        self.section_mandats = self.AjouterSection(
+            _(u"Mandats SEPA"),
+            _(u"Gérez les mandats associés au compte de la famille."),
+            proportion=1,
+        )
+        parent_mandats = self.section_mandats.GetContenu()
+
+        self.bouton_ajouter = CTRL_ActionRepens.CTRL(
+            parent_mandats,
+            label=_(u"Ajouter"),
+            icone="add",
+            variante="primaire",
+            tooltip=_(u"Ajouter un mandat"),
+        )
+        self.bouton_modifier = CTRL_ActionRepens.CTRL(
+            parent_mandats,
+            label=_(u"Modifier"),
+            icone="edit",
+            tooltip=_(u"Modifier le mandat sélectionné"),
+        )
+        self.bouton_supprimer = CTRL_ActionRepens.CTRL(
+            parent_mandats,
+            label=_(u"Supprimer"),
+            icone="delete",
+            variante="danger",
+            tooltip=_(u"Supprimer le mandat sélectionné"),
+        )
+
+        ligne_actions = wx.BoxSizer(wx.HORIZONTAL)
+        ligne_actions.AddStretchSpacer(1)
+        ligne_actions.Add(self.bouton_ajouter, 0, wx.ALIGN_CENTER_VERTICAL)
+        ligne_actions.AddSpacer(Style.espace(1))
+        ligne_actions.Add(self.bouton_modifier, 0, wx.ALIGN_CENTER_VERTICAL)
+        ligne_actions.AddSpacer(Style.espace(1))
+        ligne_actions.Add(self.bouton_supprimer, 0, wx.ALIGN_CENTER_VERTICAL)
+        self.section_mandats.GetSizerContenu().Add(ligne_actions, 0, wx.EXPAND | wx.BOTTOM, Style.espace(2))
+
+        self.ctrl_listview = OL_Mandats.ListView(
+            parent_mandats,
+            id=-1,
+            IDfamille=self.IDfamille,
+            style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES | wx.LC_VRULES,
+        )
+        Style.appliquer_liste(self.ctrl_listview)
+        self.ctrl_listview.SetMinSize((Style.px(260), Style.px(180)))
         self.ctrl_listview.MAJ()
-        
-        self.bouton_ajouter = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Ajouter.png"), wx.BITMAP_TYPE_ANY))
-        self.bouton_modifier = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Modifier.png"), wx.BITMAP_TYPE_ANY))
-        self.bouton_supprimer = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Supprimer.png"), wx.BITMAP_TYPE_ANY))
+        self.section_mandats.GetSizerContenu().Add(self.ctrl_listview, 1, wx.EXPAND)
 
-        # Boutons
-        self.bouton_aide = CTRL_Bouton_image.CTRL(self, texte=_(u"Aide"), cheminImage="Images/32x32/Aide.png")
-        self.bouton_rib = wx.Button(self, -1, _(u"Saisie d'un RIB (Prélèvements nationaux)"))
-        self.bouton_fermer = CTRL_Bouton_image.CTRL(self, texte=_(u"Fermer"), cheminImage="Images/32x32/Fermer.png")
+        # Footer commun ----------------------------------------------------
+        self.bouton_aide = self.AjouterAction(
+            _(u"Aide"),
+            callback=self.OnBoutonAide,
+            icone="help",
+            alignement="gauche",
+            tooltip=_(u"Obtenir de l'aide"),
+        )
+        self.bouton_rib = self.AjouterAction(
+            _(u"Ancien RIB"),
+            callback=self.OnBoutonRib,
+            icone="document",
+            alignement="gauche",
+            tooltip=_(u"Paramétrer un RIB pour les anciens prélèvements nationaux"),
+        )
+        self.bouton_fermer = self.AjouterAction(
+            _(u"Valider et fermer"),
+            callback=self.OnBoutonFermer,
+            icone="check",
+            variante="primaire",
+            alignement="droite",
+            tooltip=_(u"Enregistrer l'activation et fermer"),
+        )
 
-        self.__set_properties()
-        self.__do_layout()
-        
-        # Binds
-        self.Bind(wx.EVT_BUTTON, self.Ajouter, self.bouton_ajouter)
-        self.Bind(wx.EVT_BUTTON, self.Modifier, self.bouton_modifier)
-        self.Bind(wx.EVT_BUTTON, self.Supprimer, self.bouton_supprimer)
-        self.Bind(wx.EVT_BUTTON, self.OnBoutonAide, self.bouton_aide)
-        self.Bind(wx.EVT_BUTTON, self.OnBoutonRib, self.bouton_rib)
-        self.Bind(wx.EVT_BUTTON, self.OnBoutonFermer, self.bouton_fermer)
+        self.radio_activation_oui.SetToolTip(wx.ToolTip(_(u"Activer le prélèvement")))
+        self.radio_activation_non.SetToolTip(wx.ToolTip(_(u"Désactiver le prélèvement")))
+
+        self.bouton_ajouter.Bind(wx.EVT_BUTTON, self.Ajouter)
+        self.bouton_modifier.Bind(wx.EVT_BUTTON, self.Modifier)
+        self.bouton_supprimer.Bind(wx.EVT_BUTTON, self.Supprimer)
         self.Bind(wx.EVT_CLOSE, self.OnBoutonFermer)
-        
-        # Init contrôles
+
         self.Importation()
-
-    def __set_properties(self):
-        self.radio_activation_oui.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour activer le prélèvement")))
-        self.radio_activation_non.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour désactiver le prélèvement")))
-        self.bouton_ajouter.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour ajouter un mandat")))
-        self.bouton_modifier.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour modifier le mandat sélectionné dans la liste")))
-        self.bouton_supprimer.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour supprimer le mandat sélectionné dans la iste")))
-        self.bouton_aide.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour obtenir de l'aide")))
-        self.bouton_rib.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour paramétrer un RIB pour les prélèvements nationaux (jusqu'au 1er février 2014)")))
-        self.bouton_fermer.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour fermer")))
-        self.SetMinSize((680, 500))
-
-    def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=6, cols=1, vgap=10, hgap=10)
-        grid_sizer_base.Add(self.ctrl_bandeau, 0, wx.EXPAND, 0)
-
-        # Activation
-        box_activation = wx.StaticBoxSizer(self.box_activation_staticbox, wx.VERTICAL)
-        grid_sizer_activation = wx.FlexGridSizer(rows=1, cols=3, vgap=10, hgap=10)
-        grid_sizer_activation.Add(self.label_activation, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_activation.Add(self.radio_activation_oui, 0, wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_activation.Add(self.radio_activation_non, 0, wx.ALIGN_CENTER_VERTICAL, 0)
-        box_activation.Add(grid_sizer_activation, 1, wx.ALL|wx.EXPAND, 10)
-        
-        grid_sizer_base.Add(box_activation, 1, wx.LEFT|wx.RIGHT|wx.EXPAND, 10)
-        
-        # Mandats
-        box_mandats = wx.StaticBoxSizer(self.box_mandats_staticbox, wx.VERTICAL)
-        grid_sizer_mandats = wx.FlexGridSizer(rows=1, cols=2, vgap=5, hgap=5)
-        
-        grid_sizer_mandats.Add(self.ctrl_listview, 0, wx.EXPAND, 0)
-        
-        grid_sizer_droit = wx.FlexGridSizer(rows=4, cols=1, vgap=5, hgap=5)
-        grid_sizer_droit.Add(self.bouton_ajouter, 0, 0, 0)
-        grid_sizer_droit.Add(self.bouton_modifier, 0, 0, 0)
-        grid_sizer_droit.Add(self.bouton_supprimer, 0, 0, 0)
-        grid_sizer_mandats.Add(grid_sizer_droit, 1, wx.EXPAND, 0)
-        
-        grid_sizer_mandats.AddGrowableRow(0)
-        grid_sizer_mandats.AddGrowableCol(0)
-        box_mandats.Add(grid_sizer_mandats, 1, wx.ALL|wx.EXPAND, 10)
-        
-        grid_sizer_base.Add(box_mandats, 1, wx.LEFT|wx.RIGHT|wx.EXPAND, 10)
-        
-        # Boutons
-        grid_sizer_boutons = wx.FlexGridSizer(rows=1, cols=4, vgap=10, hgap=10)
-        grid_sizer_boutons.Add(self.bouton_aide, 0, wx.EXPAND, 0)
-        grid_sizer_boutons.Add(self.bouton_rib, 0, wx.EXPAND, 0)
-        grid_sizer_boutons.Add((20, 20), 0, wx.EXPAND, 0)
-        grid_sizer_boutons.Add(self.bouton_fermer, 0, wx.EXPAND, 0)
-        grid_sizer_boutons.AddGrowableCol(2)
-        grid_sizer_base.Add(grid_sizer_boutons, 1, wx.LEFT|wx.RIGHT|wx.BOTTOM|wx.EXPAND, 10)
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
-        grid_sizer_base.AddGrowableCol(0)
-        grid_sizer_base.AddGrowableRow(2)
-        self.Layout()
-        self.CenterOnScreen() 
+        self.Finaliser()
 
     def OnBoutonAide(self, event):
         from Utils import UTILS_Aide
@@ -132,7 +159,7 @@ class Dialog(wx.Dialog):
 
     def Ajouter(self, event):
         self.ctrl_listview.Ajouter(None)
-        
+
     def Modifier(self, event):
         self.ctrl_listview.Modifier(None)
 
@@ -140,70 +167,63 @@ class Dialog(wx.Dialog):
         self.ctrl_listview.Supprimer(None)
 
     def Importation(self):
-        """ Importation des données """
-        if self.IDfamille == None :
+        """Importation des données."""
+        if self.IDfamille is None:
             return
         DB = GestionDB.DB()
         req = """SELECT IDfamille, prelevement_activation
-        FROM familles 
+        FROM familles
         WHERE IDfamille=%d;""" % self.IDfamille
         DB.ExecuterReq(req)
         listeDonnees = DB.ResultatReq()
         DB.Close()
-        if len(listeDonnees) == 0 : return
-        temp, activation = listeDonnees[0]
-        
-        if activation == 1 :
+        if len(listeDonnees) == 0:
+            return
+        _temp, activation = listeDonnees[0]
+        if activation == 1:
             self.radio_activation_oui.SetValue(True)
-        
 
     def OnBoutonFermer(self, event):
-        # Récupération des données
         activation = self.radio_activation_oui.GetValue()
 
-        # Vérification des données saisies
-        if activation == True :
-            if len(self.ctrl_listview.donnees) == 0 :
-                dlg = wx.MessageDialog(self, _(u"Vous devez saisir au moins un mandat pour pouvoir activer le prélèvement !"), _(u"Erreur de saisie"), wx.OK | wx.ICON_EXCLAMATION)
+        if activation is True:
+            if len(self.ctrl_listview.donnees) == 0:
+                dlg = wx.MessageDialog(
+                    self,
+                    _(u"Vous devez saisir au moins un mandat pour pouvoir activer le prélèvement !"),
+                    _(u"Erreur de saisie"),
+                    wx.OK | wx.ICON_EXCLAMATION,
+                )
                 dlg.ShowModal()
                 dlg.Destroy()
                 return
-
-        else :
-            # Pas d'activation
-            dlg = wx.MessageDialog(None, _(u"Vous confirmez que vous ne souhaitez pas activer le prélèvement automatique ?"), _(u"Confirmation"), wx.YES_NO|wx.YES_DEFAULT|wx.CANCEL|wx.ICON_QUESTION)
-            reponse = dlg.ShowModal() 
+        else:
+            dlg = wx.MessageDialog(
+                self,
+                _(u"Vous confirmez que vous ne souhaitez pas activer le prélèvement automatique ?"),
+                _(u"Confirmation"),
+                wx.YES_NO | wx.YES_DEFAULT | wx.CANCEL | wx.ICON_QUESTION,
+            )
+            reponse = dlg.ShowModal()
             dlg.Destroy()
-            if reponse != wx.ID_YES :
+            if reponse != wx.ID_YES:
                 return
 
-        # Sauvegarde
-        if activation == True :
-            activation = 1
-        else :
-            activation = None
-            
+        activation = 1 if activation is True else None
         DB = GestionDB.DB()
         DB.ReqMAJ("familles", [("prelevement_activation", activation),], "IDfamille", self.IDfamille)
         DB.Close()
-
-        # Fermeture
         self.EndModal(wx.ID_OK)
 
     def OnBoutonRib(self, event):
         from Dlg import DLG_Saisie_rib
-        dlg = DLG_Saisie_rib.Dialog(self, IDfamille=self.IDfamille) 
-        dlg.ShowModal() 
+        dlg = DLG_Saisie_rib.Dialog(self, IDfamille=self.IDfamille)
+        dlg.ShowModal()
         dlg.Destroy()
-        
-        
-        
-
 
 
 if __name__ == u"__main__":
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     dialog_1 = Dialog(None, IDfamille=14)
     app.SetTopWindow(dialog_1)
     dialog_1.ShowModal()
