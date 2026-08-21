1fa89468bf6a9907ff2d053369ada2940ed3a7a8
fr4nck <f.bellardie@gmail.com>
2026-08-21 07:47:52 +0200

    ui: route document model choice through Repens stylesheet

--- noethys/Ctrl/CTRL_Choix_modele.py
+++ noethys/Ctrl/CTRL_Choix_modele.py
@@ -5,42 +5,26 @@
 # Site internet :  www.noethys.com
 # Auteur:           Ivan LUCAS
 # Copyright:       (c) 2010-11 Ivan LUCAS
-# Licence:         Licence GNU GPL
+# Licence:          Licence GNU GPL
 #-----------------------------------------------------------
 
 import wx
 import GestionDB
 
 from Utils.UTILS_Traduction import _
-from Utils import UTILS_Interface
-from Utils import UTILS_UIMetrics
+from Utils import UTILS_StyleRepens as Style
 
 
 class CTRL_Choice(wx.Choice):
-    """Choix d'un modèle de document avec métriques desktop communes."""
+    """Choix d'un modèle de document aligné sur le CSS Repens."""
 
     def __init__(self, parent, categorie=""):
         wx.Choice.__init__(self, parent, -1)
         self.parent = parent
         self.categorie = categorie
         self.defaut = None
-        self._AppliqueStyle()
+        Style.appliquer_saisie(self)
         self.MAJ()
-
-    def _AppliqueStyle(self):
-        try:
-            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
-            facteur = UTILS_Interface.GetTailleTexte() / 100.0
-            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
-            self.SetFont(police)
-        except Exception:
-            pass
-        try:
-            self.SetMinSize((-1, UTILS_UIMetrics.action_target("compact")))
-            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container_lowest"))
-            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface"))
-        except Exception:
-            pass
 
     def SetCategorie(self, categorie=""):
         self.categorie = categorie
@@ -124,9 +108,10 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
+        Style.appliquer_fenetre(panel)
         self.ctrl = CTRL_Choice(panel, categorie="facture")
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.ctrl, 0, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.ctrl, 0, wx.ALL | wx.EXPAND, Style.espace(2))
         panel.SetSizer(sizer)
         cadre = wx.BoxSizer(wx.VERTICAL)
         cadre.Add(panel, 1, wx.EXPAND)
