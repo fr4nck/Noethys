65978c3e2df63166ae3c74626d5117ba5f3f7de0
fr4nck <f.bellardie@gmail.com>
2026-08-21 11:25:15 +0200

    UI: harmoniser la liste des factures avec Repens

--- noethys/Ctrl/CTRL_Liste_factures.py
+++ noethys/Ctrl/CTRL_Liste_factures.py
@@ -10,11 +10,11 @@
 
 import wx
 
-from Ctrl import CTRL_Bouton_image
+from Ctrl import CTRL_ActionRepens
+from Ctrl import CTRL_OutilsListeRepens
 from Dlg import DLG_Filtres_factures
 from Ol import OL_Factures
-from Utils import UTILS_Interface
-from Utils import UTILS_UIMetrics
+from Utils import UTILS_StyleRepens as Style
 from Utils.UTILS_Traduction import _
 
 
@@ -24,7 +24,7 @@ class CTRL(wx.Panel):
     def __init__(self, parent, filtres=[], codesColonnes=["IDfacture", "date", "numero", "famille", "prelevement", "email", "total", "solde", "solde_actuel", "date_echeance", "nom_lot"], checkColonne=True, triColonne="numero"):
         wx.Panel.__init__(self, parent, id=-1, name="CTRL_Liste_factures", style=wx.TAB_TRAVERSAL)
         self.parent = parent
-        self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+        Style.appliquer_fenetre(self, "surface")
 
         self.listviewAvecFooter = OL_Factures.ListviewAvecFooter(
             self,
@@ -34,45 +34,58 @@ class CTRL(wx.Panel):
         self.ctrl_filtres = DLG_Filtres_factures.CTRL_Filtres(self, filtres=filtres, ctrl_factures=self.ctrl_factures)
 
         self.bouton_apercu = self._CreerBouton(
-            _(u"Aperçu facture"), "Images/16x16/Apercu.png",
+            _(u"Aperçu facture"),
             _(u"Afficher un aperçu de la facture sélectionnée"),
         )
         self.bouton_email = self._CreerBouton(
-            _(u"Envoyer"), "Images/16x16/Emails_exp.png",
+            _(u"Envoyer"),
             _(u"Envoyer la facture sélectionnée par Email"),
+            icone="mail",
         )
         self.bouton_supprimer = self._CreerBouton(
-            _(u"Supprimer"), "Images/16x16/Supprimer.png",
+            _(u"Supprimer"),
             _(u"Supprimer la facture sélectionnée ou les factures cochées"),
+            icone="delete",
+            variante="danger",
         )
         self.bouton_liste_apercu = self._CreerBouton(
-            _(u"Aperçu liste"), "Images/16x16/Apercu.png",
+            _(u"Aperçu liste"),
             _(u"Afficher un aperçu avant impression de cette liste"),
         )
         self.bouton_liste_imprimer = self._CreerBouton(
-            _(u"Imprimer"), "Images/16x16/Imprimante.png",
+            _(u"Imprimer"),
             _(u"Imprimer cette liste"),
         )
         self.bouton_liste_export_texte = self._CreerBouton(
-            _(u"Texte"), "Images/16x16/Texte2.png",
+            _(u"Texte"),
             _(u"Exporter cette liste au format Texte"),
+            variante="ghost",
         )
         self.bouton_liste_export_excel = self._CreerBouton(
-            _(u"Excel"), "Images/16x16/Excel.png",
+            _(u"Excel"),
             _(u"Exporter cette liste au format Excel"),
+            variante="ghost",
         )
 
-        self.ctrl_recherche = OL_Factures.BarreRecherche(self, listview=self.ctrl_factures)
+        self.ctrl_recherche = CTRL_OutilsListeRepens.BarreRecherche(self, listview=self.ctrl_factures)
         self.ctrl_afficher_annulations = wx.CheckBox(self, -1, _(u"Afficher les factures annulées"))
         self.ctrl_afficher_annulations.SetToolTip(wx.ToolTip(_(u"Afficher les factures annulées dans la liste")))
+        Style.appliquer_texte(
+            self.ctrl_afficher_annulations,
+            role="body",
+            role_texte="on_surface",
+            role_fond="surface",
+        )
 
         self.bouton_tout = self._CreerBouton(
-            _(u"Tout cocher"), "Images/16x16/Cocher.png",
+            _(u"Tout cocher"),
             _(u"Cocher toutes les factures affichées"),
+            variante="ghost",
         )
         self.bouton_rien = self._CreerBouton(
-            _(u"Tout décocher"), "Images/16x16/Decocher.png",
+            _(u"Tout décocher"),
             _(u"Décocher toutes les factures affichées"),
+            variante="ghost",
         )
         # Alias conservés pour les éventuels appels historiques externes.
         self.hyper_tout = self.bouton_tout
@@ -91,20 +104,20 @@ class CTRL(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.OnBoutonRien, self.bouton_rien)
         self.Bind(wx.EVT_CHECKBOX, self.OnCheckAnnulations, self.ctrl_afficher_annulations)
 
-    def _CreerBouton(self, texte, image, tooltip):
-        bouton = CTRL_Bouton_image.CTRL(
+    def _CreerBouton(self, texte, tooltip, icone=None, variante="secondaire"):
+        return CTRL_ActionRepens.CTRL(
             self,
-            texte=texte,
-            cheminImage=image,
-            tailleImage=(20, 20),
+            label=texte,
+            icone=icone,
+            variante=variante,
+            tooltip=tooltip,
+            compact=True,
         )
-        bouton.SetToolTip(wx.ToolTip(tooltip))
-        return bouton
 
     def __do_layout(self):
-        marge = UTILS_UIMetrics.spacing(2)
-        espace = UTILS_UIMetrics.spacing(1)
-        separation = UTILS_UIMetrics.spacing(3)
+        marge = Style.espace(2)
+        espace = Style.espace(1)
+        separation = Style.espace(3)
 
         actions = wx.BoxSizer(wx.HORIZONTAL)
         for bouton in (self.bouton_apercu, self.bouton_email, self.bouton_supprimer):
@@ -131,7 +144,7 @@ class CTRL(wx.Panel):
         principal.Add(self.listviewAvecFooter, 1, wx.EXPAND)
         principal.Add(outils, 0, wx.EXPAND | wx.TOP, marge)
         self.SetSizer(principal)
-        self.SetMinSize((UTILS_UIMetrics.px(620), UTILS_UIMetrics.px(300)))
+        self.SetMinSize((Style.px(620), Style.px(300)))
         self.Layout()
 
     def OnBoutonApercu(self, event):
@@ -182,10 +195,11 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         panel = wx.Panel(self, -1)
+        Style.appliquer_fenetre(panel, "surface")
         self.ctrl = CTRL(panel)
         self.ctrl.MAJ()
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, UTILS_UIMetrics.spacing(2))
+        sizer.Add(self.ctrl, 1, wx.ALL | wx.EXPAND, Style.espace(2))
         panel.SetSizer(sizer)
         principal = wx.BoxSizer(wx.VERTICAL)
         principal.Add(panel, 1, wx.EXPAND)
