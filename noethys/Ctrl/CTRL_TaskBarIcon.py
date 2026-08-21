bfc31f140464892fa19117e254b3d3e07b08393d
fr4nck <f.bellardie@gmail.com>
2026-08-21 11:27:07 +0200

    UI: harmoniser le badge de zone de notification avec Repens

--- noethys/Ctrl/CTRL_TaskBarIcon.py
+++ noethys/Ctrl/CTRL_TaskBarIcon.py
@@ -8,15 +8,15 @@
 # Licence:         Licence GNU GPL
 #------------------------------------------------------------------------
 
-
 import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
 
-try :
+from Utils import UTILS_StyleRepens as Style
+from Utils.UTILS_Traduction import _
+
+try:
     from wx import TaskBarIcon as TaskBarIcon
-except :
+except Exception:
     from wx.adv import TaskBarIcon as TaskBarIcon
 
 
@@ -29,24 +29,24 @@ class CustomTaskBarIcon():
         # wx.EVT_TASKBAR_RIGHT_UP(self.tbicon, self.OnTaskBarRightClick)
 
     def Cacher(self):
-        try :
+        try:
             self.tbicon.RemoveIcon()
-        except :
+        except Exception:
             pass
 
     def Detruire(self):
-        try :
+        try:
             self.tbicon.Destroy()
-        except :
+        except Exception:
             pass
 
     def Connecthys(self, nbre=None, texte=""):
-        if nbre not in (None, 0) :
+        if nbre not in (None, 0):
             chemin_logo = Chemins.GetStaticPath("Images/16x16/Nomadhys.png")
             bmp = wx.Bitmap(chemin_logo, wx.BITMAP_TYPE_ANY)
             bmp = self.AjouteTexteImage(bmp, str(nbre), taille_police=6)
             self.SetIcone(bmp=bmp, texte=texte)
-        else :
+        else:
             self.Cacher()
 
     def SetIcone(self, bmp=None, texte=""):
@@ -57,9 +57,16 @@ class CustomTaskBarIcon():
         icon.CopyFromBitmap(bmp)
         self.tbicon.SetIcon(icon, texte)
 
+    def _PoliceBadge(self, taille_police):
+        """Police compacte dérivée de la typographie Repens."""
+        try:
+            facteur = max(0.55, float(taille_police) / 9.0)
+        except Exception:
+            facteur = 1.0
+        return Style.police("caption", bold=True, scale=facteur)
+
     def AjouteTexteImage(self, image=None, texte="", alignement="droite-bas", padding=0, taille_police=9):
-        """ Ajoute un texte sur une image bitmap """
-        # Création du bitmap
+        """Ajoute un badge sémantique sur une image bitmap."""
         largeurImage, hauteurImage = image.GetSize()
         if 'phoenix' in wx.PlatformInfo:
             bmp = wx.Bitmap(largeurImage, hauteurImage)
@@ -67,49 +74,43 @@ class CustomTaskBarIcon():
             bmp = wx.EmptyBitmap(largeurImage, hauteurImage)
         mdc = wx.MemoryDC(bmp)
         dc = wx.GCDC(mdc)
+
+        # Le noir reste uniquement la clé de transparence historique du bitmap.
         mdc.SetBackground(wx.Brush("black"))
         mdc.Clear()
 
-        # Paramètres
-        dc.SetBrush(wx.Brush(wx.RED))
+        dc.SetBrush(wx.Brush(Style.couleur("danger")))
         dc.SetPen(wx.TRANSPARENT_PEN)
-        dc.SetFont(wx.Font(taille_police, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
-        dc.SetTextForeground(wx.WHITE)
+        dc.SetFont(self._PoliceBadge(taille_police))
+        dc.SetTextForeground(Style.couleur("danger_text"))
 
-        # Calculs
         largeurTexte, hauteurTexte = dc.GetTextExtent(texte)
-
-        # Image
         mdc.DrawBitmap(image, 0, 0)
 
-        # Rond rouge
-        hauteurRond = hauteurTexte + padding * 2
-        largeurRond = largeurTexte + padding * 2 + hauteurRond/2.0
-        if largeurRond < hauteurRond :
-            largeurRond = hauteurRond
+        hauteurRond = max(1, int(round(hauteurTexte + padding * 2)))
+        largeurRond = max(
+            hauteurRond,
+            int(round(largeurTexte + padding * 2 + hauteurRond / 2.0)),
+        )
 
-        if "gauche" in alignement : xRond = 1
-        if "droite" in alignement : xRond = largeurImage - largeurRond - 1
-        if "haut" in alignement : yRond = 1
-        if "bas" in alignement : yRond = hauteurImage - hauteurRond - 1
+        xRond = 1 if "gauche" in alignement else max(0, largeurImage - largeurRond - 1)
+        yRond = 1 if "haut" in alignement else max(0, hauteurImage - hauteurRond - 1)
+        rect = wx.Rect(int(xRond), int(yRond), int(largeurRond), int(hauteurRond))
+        rayon = max(1, int(round(hauteurRond / 2.0)))
 
         if 'phoenix' in wx.PlatformInfo:
-            dc.DrawRoundedRectangle(wx.Rect(xRond, yRond, largeurRond, hauteurRond), hauteurRond / 2.0)
+            dc.DrawRoundedRectangle(rect, rayon)
         else:
-            dc.DrawRoundedRectangleRect(wx.Rect(xRond, yRond, largeurRond, hauteurRond), hauteurRond / 2.0)
+            dc.DrawRoundedRectangleRect(rect, rayon)
 
-        # Texte
-        xTexte = xRond + largeurRond / 2.0 - largeurTexte / 2.0
-        yTexte = yRond + hauteurRond / 2.0 - hauteurTexte / 2.0 - 1
+        xTexte = int(round(xRond + largeurRond / 2.0 - largeurTexte / 2.0))
+        yTexte = int(round(yRond + hauteurRond / 2.0 - hauteurTexte / 2.0 - 1))
         dc.DrawText(texte, xTexte, yTexte)
 
         mdc.SelectObject(wx.NullBitmap)
         bmp.SetMaskColour("black")
         return bmp
 
-
-
-        
 
 if __name__ == u"__main__":
     app = wx.App(0)
