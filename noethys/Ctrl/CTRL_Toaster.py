4b1e8ee57c6e8cd538725af5960162db1bf17bf5
fr4nck <f.bellardie@gmail.com>
2026-08-21 11:18:20 +0200

    UI: faire consommer Repens au toaster utilisateur

--- noethys/Ctrl/CTRL_Toaster.py
+++ noethys/Ctrl/CTRL_Toaster.py
@@ -8,60 +8,46 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
 import Chemins
-from Utils import UTILS_Adaptations
-from Utils import UTILS_Interface
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 import wx.lib.agw.toasterbox as Toaster
 
+from Utils import UTILS_StyleRepens as Style
+from Utils.UTILS_Traduction import _
 
-def _PoliceSysteme(coefficient=1.0, gras=False):
-    """Police du toaster dérivée de la police native de la plateforme."""
+
+def _CouleursFondExplicite(fond):
+    """Choisit un texte lisible lorsqu'un appelant impose un fond historique."""
     try:
-        police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
-        if hasattr(police, "GetFractionalPointSize") and hasattr(police, "SetFractionalPointSize"):
-            police.SetFractionalPointSize(max(5.0, police.GetFractionalPointSize() * coefficient))
-        else:
-            police.SetPointSize(max(5, int(round(police.GetPointSize() * coefficient))))
-        if gras:
-            police.SetWeight(wx.FONTWEIGHT_BOLD)
-        return police
+        texte = wx.BLACK if fond.GetLuminance() >= 0.55 else wx.WHITE
     except Exception:
-        return wx.NullFont
+        texte = wx.WHITE
+    return texte, texte
 
 
 def ToasterUtilisateur(parent, titre=u"", prenom=_(u"Philippe"), nomImage="Femme", taille=(200, 100), couleurFond=None):
-    """ Affiche une boîte de dialogue temporaire """
-    sombre = UTILS_Interface.EstSombre()
-
+    """Affiche une boîte de dialogue temporaire."""
     # Si un appelant fournit une couleur historique/personnalisée, elle garde
-    # la priorité. Sinon le toaster suit automatiquement le design system.
+    # la priorité. Sinon le toaster suit automatiquement Repens Design.
     if couleurFond is None:
-        fond = UTILS_Interface.GetCouleurRole("surface_container_highest", sombre=sombre)
-        texte_principal = UTILS_Interface.GetCouleurRole("on_surface", sombre=sombre)
-        texte_secondaire = UTILS_Interface.GetCouleurRole("on_surface_variant", sombre=sombre)
+        fond = Style.couleur("surface_container_highest")
+        texte_principal = Style.couleur("on_surface")
+        texte_secondaire = Style.couleur("on_surface_variant")
     else:
         fond = couleurFond
-        # Pour un fond explicite, on conserve une lecture robuste sans deviner
-        # l'intention métier de l'appelant.
-        texte_principal = wx.WHITE
-        texte_secondaire = wx.Colour(210, 210, 210)
+        texte_principal, texte_secondaire = _CouleursFondExplicite(fond)
 
-    facteur_interface = UTILS_Interface.GetEchelle() / 100.0
-    largeur = max(320, int(round(400 * facteur_interface)))
-    hauteur = max(132, int(round(148 * facteur_interface)))
+    largeur = Style.px(400)
+    hauteur = Style.px(148)
 
     tb = Toaster.ToasterBox(parent, Toaster.TB_COMPLEX, Toaster.TB_DEFAULT_STYLE, Toaster.TB_ONTIME) # TB_CAPTION
     tb.SetTitle(titre)
     tb.SetPopupSize((largeur, hauteur))
     if 'phoenix' in wx.PlatformInfo:
         largeurEcran, hauteurEcran = wx.ScreenDC().GetSize()
-    else :
+    else:
         largeurEcran, hauteurEcran = wx.ScreenDC().GetSizeTuple()
-    tb.SetPopupPosition((largeurEcran-largeur-10, hauteurEcran-hauteur-50))
+    tb.SetPopupPosition((largeurEcran-largeur-Style.espace(2), hauteurEcran-hauteur-Style.px(50)))
     tb.SetPopupPauseTime(3000)
     tb.SetPopupScrollSpeed(4)
     tb.SetPopupBackgroundColour(fond)
@@ -70,6 +56,9 @@ def ToasterUtilisateur(parent, titre=u"", prenom=_(u"Philippe"), nomImage="Femme
     tbpanel = tb.GetToasterBoxWindow()
     panel = wx.Panel(tbpanel, -1)
     panel.SetBackgroundColour(fond)
+    panel.SetForegroundColour(texte_principal)
+    panel.SetFont(Style.police("body"))
+
     sizer = wx.BoxSizer(wx.VERTICAL)
     sizerHoriz = wx.BoxSizer(wx.HORIZONTAL)
     sizerTexte = wx.BoxSizer(wx.VERTICAL)
@@ -80,42 +69,31 @@ def ToasterUtilisateur(parent, titre=u"", prenom=_(u"Philippe"), nomImage="Femme
         bmp.SetBackgroundColour(fond)
     except Exception:
         pass
-    sizerHoriz.Add(bmp, 0, wx.ALL, max(6, int(round(10 * facteur_interface))))
+    sizerHoriz.Add(bmp, 0, wx.ALL, Style.espace(2))
 
-    # Texte1
+    # Texte 1
     texte1 = _(u"Bonjour")
     label1 = wx.StaticText(panel, -1, texte1, style=wx.ALIGN_CENTER)
-    label1.SetFont(_PoliceSysteme(1.05, gras=True))
+    label1.SetFont(Style.police("h5"))
     label1.SetForegroundColour(texte_secondaire)
     label1.SetBackgroundColour(fond)
-    sizerTexte.Add(label1, 1, wx.TOP | wx.EXPAND, max(24, int(round(40 * facteur_interface))))
+    sizerTexte.Add(label1, 1, wx.TOP | wx.EXPAND, Style.espace(10))
 
     # Texte 2
     texte2 = prenom
     label2 = wx.StaticText(panel, -1, texte2, style=wx.ALIGN_CENTER)
-    label2.SetFont(_PoliceSysteme(1.45, gras=True))
+    label2.SetFont(Style.police("h2"))
     label2.SetForegroundColour(texte_principal)
     label2.SetBackgroundColour(fond)
-    sizerTexte.Add(label2, 1, wx.TOP | wx.EXPAND, 0)
+    sizerTexte.Add(label2, 1, wx.EXPAND)
 
-    sizerHoriz.Add(sizerTexte, 1, wx.EXPAND, 0)
+    sizerHoriz.Add(sizerTexte, 1, wx.EXPAND)
     sizer.Add(sizerHoriz, 0, wx.EXPAND)
     panel.SetSizer(sizer)
     panel.Layout()
 
-    # Le moteur commun applique ici la taille de texte indépendante choisie par
-    # l'utilisateur, sans changer le contrat métier ni le contenu du toaster.
-    try:
-        UTILS_Interface.AppliquerAffichage(panel, recursif=True)
-        panel.SetBackgroundColour(fond)
-        label1.SetBackgroundColour(fond)
-        label2.SetBackgroundColour(fond)
-    except Exception:
-        pass
-
     tb.AddPanel(panel)
     tb.Play()
-
 
 
 if __name__ == '__main__':
