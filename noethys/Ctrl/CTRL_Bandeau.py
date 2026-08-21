54619b09b3a987d7228329d5b72dd1218221fec7
fr4nck <f.bellardie@gmail.com>
2026-08-21 06:43:48 +0200

    UI: remplace le HTML historique des bandeaux par du texte responsive

--- noethys/Ctrl/CTRL_Bandeau.py
+++ noethys/Ctrl/CTRL_Bandeau.py
@@ -8,19 +8,29 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
+import html as html_std
+import re
+
 import Chemins
 from Utils import UTILS_Interface
 from Utils import UTILS_UIMetrics
 from Utils.UTILS_Traduction import _
 import wx
-import wx.html as html
 
 
-def _couleur_html(couleur):
+def _texte_simple(texte):
+    """Convertit les quelques balises historiques en texte natif lisible."""
+    if texte in (None, ""):
+        return u""
+    texte = str(texte)
+    texte = re.sub(r"(?i)<\s*br\s*/?\s*>", "\n", texte)
+    texte = re.sub(r"(?i)</\s*p\s*>", "\n", texte)
+    texte = re.sub(r"<[^>]+>", "", texte)
     try:
-        return u"#%02X%02X%02X" % (couleur.Red(), couleur.Green(), couleur.Blue())
+        texte = html_std.unescape(texte)
     except Exception:
-        return u"#000000"
+        pass
+    return texte.strip()
 
 
 def _bitmap_adapte(chemin, taille):
@@ -35,57 +45,74 @@ def _bitmap_adapte(chemin, taille):
         return wx.NullBitmap
 
 
-class MyHtml(html.HtmlWindow):
-    def __init__(self, parent, texte="", hauteur=25):
-        html.HtmlWindow.__init__(
-            self,
-            parent,
-            -1,
-            style=wx.html.HW_NO_SELECTION | wx.html.HW_SCROLLBAR_NEVER | wx.NO_FULL_REPAINT_ON_RESIZE,
-        )
-        if "gtk2" in wx.PlatformInfo:
-            self.SetStandardFonts()
-        self.texte = texte
-        self.SetBorders(0)
-        self.SetMinSize((-1, max(UTILS_UIMetrics.px(hauteur), UTILS_UIMetrics.row_height("compact"))))
+class TexteIntro(wx.StaticText):
+    """Texte de bandeau natif qui se recompose avec la largeur disponible."""
+
+    def __init__(self, parent, texte=u""):
+        self.texte_original = _texte_simple(texte)
+        wx.StaticText.__init__(self, parent, -1, self.texte_original, style=wx.ST_NO_AUTORESIZE)
+        self.SetMinSize((-1, UTILS_UIMetrics.row_height("compact")))
+        self.Bind(wx.EVT_SIZE, self.OnSize)
         self.AppliquerTheme()
+        wx.CallAfter(self.Reflow)
 
     def AppliquerTheme(self):
-        sombre = UTILS_Interface.EstSombre()
-        fond = UTILS_Interface.GetCouleurRole("surface_container", sombre=sombre)
-        texte = UTILS_Interface.GetCouleurRole("on_surface_variant", sombre=sombre)
         try:
-            self.SetBackgroundColour(fond)
-            self.SetForegroundColour(texte)
+            self.SetForegroundColour(UTILS_Interface.GetCouleurRole("on_surface_variant"))
+            self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface_container"))
         except Exception:
             pass
-        self.SetPage(
-            u'<BODY BGCOLOR="%s" TEXT="%s"><FONT SIZE=-2>%s</FONT></BODY>' % (
-                _couleur_html(fond),
-                _couleur_html(texte),
-                self.texte,
-            )
-        )
+
+    def SetTexte(self, texte):
+        self.texte_original = _texte_simple(texte)
+        self.Reflow()
+
+    def Reflow(self):
+        try:
+            largeur = int(self.GetClientSize().GetWidth())
+        except Exception:
+            largeur = 0
+        self.SetLabel(self.texte_original)
+        if largeur > UTILS_UIMetrics.px(120):
+            try:
+                self.Wrap(max(UTILS_UIMetrics.px(120), largeur - UTILS_UIMetrics.spacing(1)))
+            except Exception:
+                pass
+        try:
+            self.InvalidateBestSize()
+            parent = self.GetParent()
+            if parent is not None:
+                parent.Layout()
+        except Exception:
+            pass
+
+    def OnSize(self, event):
+        event.Skip()
+        wx.CallAfter(self.Reflow)
 
 
 class Bandeau(wx.Panel):
     """En-tête commun des dialogues Noethys, compact et réellement responsive."""
 
     def __init__(self, parent, titre="", texte="", hauteurHtml=25, nomImage=None):
-        wx.Panel.__init__(self, parent, id=-1, style=wx.TAB_TRAVERSAL)
+        wx.Panel.__init__(self, parent, id=-1, style=wx.TAB_TRAVERSAL | wx.BORDER_NONE)
         self.nomImage = nomImage
         self.image = None
+        # ``hauteurHtml`` reste accepté pour compatibilité API, mais aucune
+        # hauteur de texte n'est désormais figée par cette valeur.
+        self.hauteurHtml = hauteurHtml
 
         if self.nomImage is not None:
             taille = UTILS_UIMetrics.icon_size("hero")
             self.image = wx.StaticBitmap(self, -1, _bitmap_adapte(self.nomImage, taille))
 
         self.ctrl_titre = wx.StaticText(self, -1, titre)
-        self.ctrl_intro = MyHtml(self, texte, hauteurHtml)
+        self.ctrl_intro = TexteIntro(self, texte)
         self.ligne = wx.StaticLine(self, -1)
 
         self.__set_properties()
         self.__do_layout()
+        self.Bind(wx.EVT_SIZE, self.OnSize)
 
     def __set_properties(self):
         self.AppliquerTheme()
@@ -94,7 +121,7 @@ class Bandeau(wx.Panel):
         try:
             police = wx.Font(police)
             base = max(8, police.GetPointSize())
-            facteur = (UTILS_Interface.GetTailleTexte() / 100.0)
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
             police.SetPointSize(max(9, int(round((base + 1) * facteur))))
             police.SetWeight(wx.FONTWEIGHT_SEMIBOLD if hasattr(wx, "FONTWEIGHT_SEMIBOLD") else wx.FONTWEIGHT_BOLD)
             self.ctrl_titre.SetFont(police)
@@ -102,10 +129,9 @@ class Bandeau(wx.Panel):
             pass
 
     def AppliquerTheme(self):
-        sombre = UTILS_Interface.EstSombre()
-        fond = UTILS_Interface.GetCouleurRole("surface_container", sombre=sombre)
-        texte = UTILS_Interface.GetCouleurRole("on_surface", sombre=sombre)
-        bordure = UTILS_Interface.GetCouleurRole("outline_variant", sombre=sombre)
+        fond = UTILS_Interface.GetCouleurRole("surface_container")
+        texte = UTILS_Interface.GetCouleurRole("on_surface")
+        bordure = UTILS_Interface.GetCouleurRole("outline_variant")
 
         try:
             self.SetBackgroundColour(fond)
@@ -128,10 +154,9 @@ class Bandeau(wx.Panel):
             pass
 
     def __do_layout(self):
-        """BoxSizer volontaire : le texte doit récupérer toute la largeur libre."""
+        """Le texte récupère toute la largeur libre, sans hauteur HTML fixe."""
         marge_x = UTILS_UIMetrics.spacing(3)
         marge_y = UTILS_UIMetrics.spacing(2)
-        espace = UTILS_UIMetrics.spacing(2)
 
         contenu = wx.BoxSizer(wx.HORIZONTAL)
         if self.image is not None:
@@ -139,7 +164,7 @@ class Bandeau(wx.Panel):
 
         textes = wx.BoxSizer(wx.VERTICAL)
         textes.Add(self.ctrl_titre, 0, wx.EXPAND | wx.BOTTOM, max(2, UTILS_UIMetrics.spacing(1)))
-        textes.Add(self.ctrl_intro, 1, wx.EXPAND)
+        textes.Add(self.ctrl_intro, 0, wx.EXPAND)
         contenu.Add(textes, 1, wx.EXPAND)
 
         principal = wx.BoxSizer(wx.VERTICAL)
@@ -147,8 +172,12 @@ class Bandeau(wx.Panel):
         principal.Add(self.ligne, 0, wx.EXPAND)
 
         self.SetSizer(principal)
-        self.SetMinSize((-1, max(UTILS_UIMetrics.panel_min_height("compact"), self.GetBestSize().GetHeight())))
+        self.SetMinSize((-1, UTILS_UIMetrics.panel_min_height("compact")))
         self.Layout()
+
+    def OnSize(self, event):
+        event.Skip()
+        wx.CallAfter(self.ctrl_intro.Reflow)
 
 
 class MyFrame(wx.Frame):
