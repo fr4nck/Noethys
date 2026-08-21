6e8ac8cf0a55647a955cd699181202f46fb793b4
fr4nck <f.bellardie@gmail.com>
2026-08-21 06:49:19 +0200

    UI: modernise le sélecteur de date commun

--- noethys/Dlg/DLG_calendrier_simple.py
+++ noethys/Dlg/DLG_calendrier_simple.py
@@ -8,103 +8,106 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-
-import Chemins
-from Utils import UTILS_Adaptations
-from Utils.UTILS_Traduction import _
 import wx
-from Ctrl import CTRL_Bouton_image
 
-##import  wx.calendar
-import datetime
+from Ctrl import CTRL_Bouton_image
 from Ctrl import CTRL_Calendrier
+from Utils import UTILS_Dialogs
+from Utils import UTILS_Interface
+from Utils import UTILS_UIMetrics
+from Utils.UTILS_Traduction import _
+
 
 class Dialog(wx.Dialog):
+    """Sélecteur de date commun, responsive et cohérent avec Repens."""
+
     def __init__(self, parent):
-        wx.Dialog.__init__(self, parent, -1, name="DLG_Calendrier_simple", style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MAXIMIZE_BOX|wx.MINIMIZE_BOX)
+        wx.Dialog.__init__(
+            self,
+            parent,
+            -1,
+            name="DLG_Calendrier_simple",
+            title=_(u"Sélectionner une date"),
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER | wx.MAXIMIZE_BOX | wx.MINIMIZE_BOX,
+        )
         self.parent = parent
-##        self.locale = wx.Locale(wx.LANGUAGE_FRENCH)
-        
-##        self.ctrl_calendrier = wx.calendar.CalendarCtrl(self, -1, wx.DateTime_Now(), 
-##                             style = wx.calendar.CAL_SHOW_HOLIDAYS
-##                             | wx.calendar.CAL_MONDAY_FIRST
-##                             | wx.calendar.CAL_SEQUENTIAL_MONTH_SELECTION
-##                             )
-##        self.ctrl_calendrier.Bind(wx.calendar.EVT_CALENDAR, self.OnDateSelected)
-        
-        self.ctrl_calendrier = CTRL_Calendrier.CTRL(self, afficheAujourdhui=False, typeCalendrier="annuel", afficheBoutonAnnuel=True, multiSelections=False)
+        self.SetBackgroundColour(UTILS_Interface.GetCouleurRole("surface"))
+
+        self.ctrl_calendrier = CTRL_Calendrier.CTRL(
+            self,
+            afficheAujourdhui=False,
+            typeCalendrier="annuel",
+            afficheBoutonAnnuel=True,
+            multiSelections=False,
+        )
         self.ctrl_calendrier.Bind(CTRL_Calendrier.EVT_SELECT_DATES, self.OnDateSelected)
 
-        self.bouton_ok = CTRL_Bouton_image.CTRL(self, texte=_(u"Ok"), cheminImage="Images/32x32/Valider.png")
-        self.bouton_annuler = CTRL_Bouton_image.CTRL(self, id=wx.ID_CANCEL, texte=_(u"Annuler"), cheminImage="Images/32x32/Annuler.png")
-        self.bouton_aide = CTRL_Bouton_image.CTRL(self, texte=_(u"Aide"), cheminImage="Images/32x32/Aide.png")
-        self.bouton_ok.Show(False) 
-        
-        self.SetTitle(_(u"Cliquez sur une date pour la sélectionner..."))
-        self.SetMinSize((800, 600))
-        self.bouton_aide.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour obtenir de l'aide")))
-        self.bouton_annuler.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour fermer")))
-        self.Bind(wx.EVT_BUTTON, self.OnBoutonAide, self.bouton_aide)
+        # Le bouton OK reste disponible pour compatibilité avec d'éventuels
+        # appels externes, mais la sélection d'une date valide immédiatement.
+        self.bouton_ok = CTRL_Bouton_image.CTRL(
+            self,
+            texte=_(u"Valider"),
+            iconeFluent="check",
+        )
+        self.bouton_ok.Hide()
+        self.bouton_annuler = CTRL_Bouton_image.CTRL(
+            self,
+            id=wx.ID_CANCEL,
+            texte=_(u"Annuler"),
+            iconeFluent="dismiss",
+        )
+        self.bouton_aide = CTRL_Bouton_image.CTRL(
+            self,
+            texte=_(u"Aide"),
+            iconeFluent="help",
+        )
+        self.bouton_aide.SetToolTip(wx.ToolTip(_(u"Obtenir de l'aide sur le calendrier")))
+        self.bouton_annuler.SetToolTip(wx.ToolTip(_(u"Fermer sans sélectionner de date")))
 
-        grid_sizer_base = wx.FlexGridSizer(rows=3, cols=1, vgap=0, hgap=0)
-        
-        grid_sizer_contenu = wx.FlexGridSizer(rows=2, cols=2, vgap=10, hgap=10)
-        grid_sizer_contenu.Add(self.ctrl_calendrier, 0, wx.EXPAND, 0)
-        grid_sizer_contenu.AddGrowableCol(0)
-        grid_sizer_contenu.AddGrowableRow(0)
-        grid_sizer_base.Add(grid_sizer_contenu, 1, wx.ALL|wx.EXPAND, 10)
-        
-        grid_sizer_boutons = wx.FlexGridSizer(rows=1, cols=4, vgap=10, hgap=10)
-        grid_sizer_boutons.Add(self.bouton_aide, 0, 0, 0)
-        grid_sizer_boutons.Add( (1, 1), 0, 0, 0)
-        grid_sizer_boutons.Add(self.bouton_ok, 0, 0, 0)
-        grid_sizer_boutons.Add(self.bouton_annuler, 0, 0, 0)
-        grid_sizer_boutons.AddGrowableCol(0)
-        grid_sizer_base.Add(grid_sizer_boutons, 1, wx.LEFT|wx.RIGHT|wx.BOTTOM|wx.EXPAND, 10)
-        
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
-        grid_sizer_base.AddGrowableCol(0)
-        grid_sizer_base.AddGrowableRow(0)
-        self.Layout()
-        self.CenterOnScreen()
-        
+        self.Bind(wx.EVT_BUTTON, self.OnBoutonAide, self.bouton_aide)
         self.Bind(wx.EVT_BUTTON, self.OnBoutonOk, self.bouton_ok)
 
-    def OnBoutonAide(self, event): 
+        marge = UTILS_UIMetrics.spacing(3)
+        principal = wx.BoxSizer(wx.VERTICAL)
+        principal.Add(self.ctrl_calendrier, 1, wx.EXPAND | wx.ALL, marge)
+
+        actions = wx.BoxSizer(wx.HORIZONTAL)
+        actions.Add(self.bouton_aide, 0)
+        actions.AddStretchSpacer(1)
+        actions.Add(self.bouton_ok, 0, wx.LEFT, UTILS_UIMetrics.spacing(1))
+        actions.Add(self.bouton_annuler, 0, wx.LEFT, UTILS_UIMetrics.spacing(1))
+        principal.Add(actions, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, marge)
+
+        self.SetSizer(principal)
+        self.SetMinSize((UTILS_UIMetrics.px(520), UTILS_UIMetrics.px(380)))
+        self.SetSize((UTILS_UIMetrics.px(760), UTILS_UIMetrics.px(560)))
+        UTILS_Dialogs.AjusteDansEcran(self)
+        self.Layout()
+        if parent is not None:
+            self.CentreOnParent()
+        else:
+            self.CentreOnScreen()
+
+    def OnBoutonAide(self, event):
         from Utils import UTILS_Aide
         UTILS_Aide.Aide("Calendrier1")
 
     def OnDateSelected(self, event):
         self.EndModal(wx.ID_OK)
-    
-##    def GetDate(self):
-##        dateTmp = self.ctrl_calendrier.GetDate()
-##        jour = dateTmp.GetDay()
-##        mois = dateTmp.GetMonth()+1
-##        annee = dateTmp.GetYear()
-##        date = datetime.date(annee, mois, jour)
-##        return date
 
     def GetDate(self):
-        selections = self.ctrl_calendrier.GetSelections() 
-        if len(selections) > 0 :
-            return selections[0]
-        else:
-            return None
-    
+        selections = self.ctrl_calendrier.GetSelections()
+        return selections[0] if selections else None
+
     def SetDate(self, date=None):
-        self.ctrl_calendrier.SelectJours([date,])
+        self.ctrl_calendrier.SelectJours([date])
 
     def OnBoutonOk(self, event):
         self.EndModal(wx.ID_OK)
-        
-
 
 
 if __name__ == u"__main__":
     app = wx.App(0)
-    #wx.InitAllImageHandlers()
     dialog_1 = Dialog(None)
     app.SetTopWindow(dialog_1)
     dialog_1.ShowModal()
