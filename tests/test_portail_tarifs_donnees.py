aa5113ae66a7a535089b742dca1ef23796145ba6
fr4nck <f.bellardie@gmail.com>
2026-08-21 06:34:53 +0200

    Tests: garantit l'inclusion automatique des nouvelles activités

--- tests/test_portail_tarifs_donnees.py
+++ tests/test_portail_tarifs_donnees.py
@@ -17,8 +17,9 @@ from Utils import UTILS_Portail_tarifs_donnees as DONNEES
 
 
 class FakeDB(object):
-    def __init__(self):
+    def __init__(self, nouvelle_activite=False):
         self.requetes = []
+        self.nouvelle_activite = nouvelle_activite
 
     def ExecuterReq(self, requete):
         self.requetes.append(requete)
@@ -27,18 +28,25 @@ class FakeDB(object):
     def ResultatReq(self):
         requete = self.requetes[-1]
         if "FROM activites" in requete:
-            return [(1, "ALSH Bais"), (2, "École multisports")]
+            lignes = [(1, "ALSH Bais"), (2, "École multisports")]
+            if self.nouvelle_activite:
+                lignes.append((3, "Sport-Santé"))
+            return lignes
         if "FROM categories_tarifs" in requete:
-            return [(10, 1, "QF A"), (11, 1, "QF B"), (20, 2, "EMS")]
+            lignes = [(10, 1, "QF A"), (11, 1, "QF B"), (20, 2, "EMS")]
+            if self.nouvelle_activite:
+                lignes.append((30, 3, "Sport-Santé"))
+            return lignes
         if "FROM tarifs_lignes" in requete:
             champs = [champ[0] for champ in DB_DATA["tarifs_lignes"]]
-            def ligne(IDtarif, num_ligne, qf_min, qf_max, montant):
+
+            def ligne(IDtarif, IDactivite, code, num_ligne, qf_min, qf_max, montant):
                 valeurs = {champ: None for champ in champs}
                 valeurs.update({
                     "IDligne": IDtarif * 100 + num_ligne,
-                    "IDactivite": 1,
+                    "IDactivite": IDactivite,
                     "IDtarif": IDtarif,
-                    "code": "qf",
+                    "code": code,
                     "num_ligne": num_ligne,
                     "qf_min": qf_min,
                     "qf_max": qf_max,
@@ -46,16 +54,20 @@ class FakeDB(object):
                     "IDmodele": None,
                 })
                 return tuple(valeurs[champ] for champ in champs)
-            return [
-                ligne(100, 1, 0, 699, 9.35),
-                ligne(100, 2, 700, 999, 10.00),
-                ligne(200, 1, None, None, 89.00),
+
+            lignes = [
+                ligne(100, 1, "qf", 1, 0, 699, 9.35),
+                ligne(100, 1, "qf", 2, 700, 999, 10.00),
+                ligne(200, 2, "montant_unique", 1, None, None, 89.00),
             ]
+            if self.nouvelle_activite:
+                lignes.append(ligne(300, 3, "montant_unique", 1, None, None, 109.00))
+            return lignes
         if "FROM questionnaire_filtres" in requete:
             return [(501, 100)]
         if "FROM tarifs" in requete:
             # Ordre strictement identique à CHAMPS_TARIFS.
-            return [
+            lignes = [
                 (
                     100, 1, 1000, "Journée", "2026-09-01", "2027-08-31",
                     0, 0, 0, 0, 0, "qf",
@@ -71,6 +83,15 @@ class FakeDB(object):
                     None, None, 1, "Licence annuelle", "EMS", "", None,
                 ),
             ]
+            if self.nouvelle_activite:
+                lignes.append((
+                    300, 3, 3000, "Sport-Santé annuel", "2026-09-01", "2027-08-31",
+                    0, 0, 0, 0, 0, "montant_unique",
+                    "30", None, None, "FORFAIT", None, None,
+                    None, None, None, None, "706", None, 0,
+                    None, None, 1, "Adhésion Sport-Santé", "Sport-Santé", "", None,
+                ))
+            return lignes
         raise AssertionError("Requête inattendue : %s" % requete)
 
 
@@ -80,6 +101,14 @@ class PortailTarifsDonneesTests(unittest.TestCase):
         self.assertEqual(DONNEES.convertir_liste_ids("1;2;3"), [1, 2, 3])
         self.assertEqual(DONNEES.convertir_liste_ids("reservation;present", type_texte=True), ["reservation", "present"])
         self.assertIsNone(DONNEES.convertir_liste_ids(""))
+
+    def test_politique_automatique_est_le_defaut(self):
+        politique = DONNEES.normaliser_politique()
+        self.assertEqual(politique, {
+            "mode": "automatique",
+            "IDsactivites": [],
+            "IDsactivites_exclues": [],
+        })
 
     def test_charge_les_baremes_et_les_developpe_par_categorie(self):
         tarifs = DONNEES.charger_baremes(FakeDB())
@@ -107,11 +136,33 @@ class PortailTarifsDonneesTests(unittest.TestCase):
         self.assertEqual(ems["lignes_calcul"][0]["montant_unique"], 89.0)
         self.assertEqual(ems["filtres"], [])
 
-    def test_filtre_par_activite_est_applique_apres_lecture(self):
+    def test_selection_explicite_reste_disponible(self):
         tarifs = DONNEES.charger_baremes(FakeDB(), IDsactivites=[2])
         self.assertEqual(len(tarifs), 1)
         self.assertEqual(tarifs[0]["IDactivite"], 2)
         self.assertEqual(DONNEES.charger_baremes(FakeDB(), IDsactivites=[]), [])
+
+    def test_nouvelle_activite_tarifee_est_incluse_sans_modifier_la_configuration(self):
+        publication = DONNEES.construire_publication(
+            FakeDB(nouvelle_activite=True),
+            date_reference=datetime.date(2026, 9, 2),
+        )
+
+        self.assertEqual(publication["politique"]["mode"], "automatique")
+        self.assertIn(3, publication["IDsactivites_publiees"])
+        self.assertIn("Sport-Santé", publication["html"])
+        self.assertIn("109,00 €", publication["html"])
+
+    def test_exclusion_explicite_survit_a_la_decouverte_automatique(self):
+        publication = DONNEES.construire_publication(
+            FakeDB(nouvelle_activite=True),
+            politique={"mode": "automatique", "IDsactivites_exclues": [3]},
+            date_reference=datetime.date(2026, 9, 2),
+        )
+
+        self.assertNotIn(3, publication["IDsactivites_publiees"])
+        self.assertNotIn("Sport-Santé", publication["html"])
+        self.assertIn("École multisports", publication["html"])
 
     def test_publication_relit_la_source_noethys_et_signale_les_conditions(self):
         publication = DONNEES.construire_publication(
