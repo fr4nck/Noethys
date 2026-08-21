2d390c278a1213577aa8fec16d0faae9815124ce
fr4nck <f.bellardie@gmail.com>
2026-08-21 07:30:17 +0200

    UI: remplace le résumé HTML du compte internet par des contrôles natifs

--- noethys/Ctrl/CTRL_Compte_internet.py
+++ noethys/Ctrl/CTRL_Compte_internet.py
@@ -8,130 +8,158 @@
 # Licence:         Licence GNU GPL
 #-----------------------------------------------------------
 
-import html as html_std
-
 import wx
-import wx.html as wxhtml
 
-import Chemins
 from Utils import UTILS_Internet
 from Utils import UTILS_Interface
 from Utils import UTILS_UIMetrics
 from Utils.UTILS_Traduction import _
 
 
-def _CouleurHtml(couleur):
-    try:
-        return "#%02X%02X%02X" % (couleur.Red(), couleur.Green(), couleur.Blue())
-    except Exception:
-        return "#202020"
+class CTRL(wx.Panel):
+    """Résumé natif du compte internet.
 
-
-class CTRL(wxhtml.HtmlWindow):
-    """Résumé compact du compte internet, compatible thèmes et grosse police."""
+    L'ancien ``HtmlWindow`` n'apportait aucune fonction web : il servait
+    uniquement à mettre en page un statut, un identifiant et un mot de passe.
+    Des contrôles wx natifs donnent désormais un vrai reflow, une meilleure
+    accessibilité et un rendu cohérent clair/sombre tout en conservant l'API
+    métier historique de ce contrôle.
+    """
 
     def __init__(self, parent, IDfamille=None, IDutilisateur=None, couleurFond=None):
-        wxhtml.HtmlWindow.__init__(
-            self,
-            parent,
-            -1,
-            style=wx.BORDER_THEME | wxhtml.HW_NO_SELECTION | wx.NO_FULL_REPAINT_ON_RESIZE,
-        )
+        wx.Panel.__init__(self, parent, -1, style=wx.BORDER_NONE | wx.TAB_TRAVERSAL)
         self.parent = parent
         self.IDfamille = IDfamille
         self.IDutilisateur = IDutilisateur
         self.couleurFond = couleurFond or UTILS_Interface.GetCouleurRole("surface_container_lowest")
         self.dictDonnees = {}
 
-        try:
-            police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
-            facteur = UTILS_Interface.GetTailleTexte() / 100.0
-            taille = max(8, int(round(police.GetPointSize() * facteur)))
-            self.SetStandardFonts(taille, police.GetFaceName(), police.GetFaceName())
-        except Exception:
-            if "gtk2" in wx.PlatformInfo:
-                self.SetStandardFonts()
+        self.indicateur_statut = wx.StaticText(self, -1, u"●")
+        self.label_statut = wx.StaticText(self, -1, "")
+        self.label_identifiant = wx.StaticText(self, -1, _(u"Identifiant"))
+        self.valeur_identifiant = wx.StaticText(self, -1, "")
+        self.label_mdp = wx.StaticText(self, -1, _(u"Mot de passe"))
+        self.valeur_mdp = wx.StaticText(self, -1, "")
+        self.note_mdp = wx.StaticText(self, -1, _(u"Mot de passe personnalisé"))
 
-        self.SetBackgroundColour(self.couleurFond)
+        self._ConfigurerStyle()
+        self._ConstruireLayout()
         self.SetMinSize((UTILS_UIMetrics.px(220), UTILS_UIMetrics.panel_min_height("dashboard")))
-        self.SetBorders(UTILS_UIMetrics.spacing(2))
+        self.Bind(wx.EVT_SIZE, self.OnSize)
 
-    def SetDonnees(self, dictDonnees={}):
-        self.dictDonnees = dictDonnees
+    def _ConfigurerStyle(self):
+        self.SetBackgroundColour(self.couleurFond)
+        texte = UTILS_Interface.GetCouleurRole("on_surface")
+        secondaire = UTILS_Interface.GetCouleurRole("on_surface_variant")
+
+        police = wx.Font(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
+        try:
+            facteur = UTILS_Interface.GetTailleTexte() / 100.0
+            police.SetPointSize(max(8, int(round(police.GetPointSize() * facteur))))
+        except Exception:
+            pass
+
+        police_statut = wx.Font(police)
+        try:
+            police_statut.SetWeight(wx.FONTWEIGHT_SEMIBOLD)
+        except Exception:
+            police_statut.SetWeight(wx.FONTWEIGHT_BOLD)
+
+        for controle in (self.label_identifiant, self.label_mdp, self.note_mdp):
+            controle.SetForegroundColour(secondaire)
+            controle.SetFont(police)
+        for controle in (self.valeur_identifiant, self.valeur_mdp):
+            controle.SetForegroundColour(texte)
+            controle.SetFont(police)
+        self.label_statut.SetFont(police_statut)
+        self.indicateur_statut.SetFont(police_statut)
+        self.note_mdp.Hide()
+
+    def _ConstruireLayout(self):
+        espace = UTILS_UIMetrics.spacing(1)
+        marge = UTILS_UIMetrics.spacing(2)
+
+        statut = wx.BoxSizer(wx.HORIZONTAL)
+        statut.Add(self.indicateur_statut, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, espace)
+        statut.Add(self.label_statut, 1, wx.ALIGN_CENTER_VERTICAL)
+
+        infos = wx.BoxSizer(wx.VERTICAL)
+        infos.Add(self.label_identifiant, 0, wx.TOP, espace)
+        infos.Add(self.valeur_identifiant, 0, wx.EXPAND | wx.BOTTOM, espace)
+        infos.Add(self.label_mdp, 0, wx.TOP, espace)
+        infos.Add(self.valeur_mdp, 0, wx.EXPAND)
+        infos.Add(self.note_mdp, 0, wx.TOP, espace)
+
+        principal = wx.BoxSizer(wx.VERTICAL)
+        principal.Add(statut, 0, wx.EXPAND | wx.ALL, marge)
+        principal.Add(infos, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, marge)
+        self.SetSizer(principal)
+        self.Layout()
+
+    def _WrapValeurs(self):
+        try:
+            largeur = max(UTILS_UIMetrics.px(120), self.GetClientSize().GetWidth() - UTILS_UIMetrics.spacing(4))
+        except Exception:
+            return
+        for controle in (self.label_statut, self.valeur_identifiant, self.valeur_mdp, self.note_mdp):
+            try:
+                controle.Wrap(largeur)
+            except Exception:
+                pass
+
+    def OnSize(self, event):
+        event.Skip()
+        wx.CallAfter(self._WrapValeurs)
+
+    def SetDonnees(self, dictDonnees=None):
+        self.dictDonnees = dictDonnees or {}
         self.MAJ()
 
     def GetDonnees(self):
         return self.dictDonnees
 
+    def _LireMdp(self):
+        mdp = self.dictDonnees.get("internet_mdp") or ""
+        if mdp.startswith("#@#"):
+            mdp = UTILS_Internet.DecrypteMDP(mdp)
+        personnalise = mdp.startswith("custom")
+        return (_(u"********") if personnalise else mdp), personnalise
+
     def MAJ(self):
         if not self.dictDonnees:
-            self.SetPage("")
+            self.label_statut.SetLabel("")
+            self.valeur_identifiant.SetLabel("")
+            self.valeur_mdp.SetLabel("")
+            self.note_mdp.Hide()
+            self.indicateur_statut.Hide()
+            self.Layout()
             return
 
         actif = self.dictDonnees.get("internet_actif") == 1
         if actif:
             activation = _(u"Compte internet activé")
-            image = "Ok.png"
             role_statut = "success"
         else:
             activation = _(u"Compte internet désactivé")
-            image = "Interdit.png"
             role_statut = "danger"
 
         identifiant = self.dictDonnees.get("internet_identifiant") or ""
-        mdp = self.dictDonnees.get("internet_mdp") or ""
-        if mdp.startswith("#@#"):
-            mdp = UTILS_Internet.DecrypteMDP(mdp)
+        mdp, personnalise = self._LireMdp()
 
-        mdp_personnalise = mdp.startswith("custom")
-        if mdp_personnalise:
-            mdp = _(u"********")
-
-        identifiant_html = html_std.escape(str(identifiant))
-        mdp_html = html_std.escape(str(mdp))
-        activation_html = html_std.escape(str(activation))
-        note_mdp = ""
-        if mdp_personnalise:
-            note_mdp = u"<BR><FONT COLOR=\"%s\">%s</FONT>" % (
-                _CouleurHtml(UTILS_Interface.GetCouleurRole("on_surface_variant")),
-                html_std.escape(_(u"Mot de passe personnalisé")),
-            )
-
-        taille_icone = UTILS_UIMetrics.icon_size("inline")
-        chemin_icone = Chemins.GetStaticIconPath("Images/16x16/%s" % image, taille=taille_icone)
-        couleur_texte = _CouleurHtml(UTILS_Interface.GetCouleurRole("on_surface"))
-        couleur_secondaire = _CouleurHtml(UTILS_Interface.GetCouleurRole("on_surface_variant"))
-        couleur_statut = _CouleurHtml(UTILS_Interface.GetCouleurRole(role_statut))
-
-        self.SetPage(u"""
-        <CENTER>
-        <IMG SRC="%s"><BR>
-        <B><FONT COLOR="%s">%s</FONT></B>
-        <BR><BR>
-        <FONT COLOR="%s"><B>%s</B></FONT><BR>
-        <FONT COLOR="%s">%s</FONT>
-        <BR><BR>
-        <FONT COLOR="%s"><B>%s</B></FONT><BR>
-        <FONT COLOR="%s">%s</FONT>%s
-        </CENTER>
-        """ % (
-            chemin_icone,
-            couleur_statut,
-            activation_html,
-            couleur_secondaire,
-            html_std.escape(_(u"Identifiant")),
-            couleur_texte,
-            identifiant_html,
-            couleur_secondaire,
-            html_std.escape(_(u"Mot de passe")),
-            couleur_texte,
-            mdp_html,
-            note_mdp,
-        ))
-        self.SetBackgroundColour(self.couleurFond)
+        couleur_statut = UTILS_Interface.GetCouleurRole(role_statut)
+        self.indicateur_statut.SetForegroundColour(couleur_statut)
+        self.label_statut.SetForegroundColour(couleur_statut)
+        self.indicateur_statut.Show()
+        self.label_statut.SetLabel(str(activation))
+        self.valeur_identifiant.SetLabel(str(identifiant))
+        self.valeur_mdp.SetLabel(str(mdp))
+        self.note_mdp.Show(bool(personnalise))
+        self._WrapValeurs()
+        self.Layout()
+        self.Refresh()
 
     def GetIdentifiant(self):
-        return self.dictDonnees["internet_identifiant"]
+        return self.dictDonnees.get("internet_identifiant")
 
     def GetMdp(self):
         internet_mdp = self.dictDonnees.get("internet_mdp") or ""
@@ -154,9 +182,11 @@ class CTRL(wxhtml.HtmlWindow):
         codes = _(u"Identifiant : %s / Mot de passe : %s") % (self.GetIdentifiant(), self.GetMdp())
         clipdata = wx.TextDataObject()
         clipdata.SetText(codes)
-        wx.TheClipboard.Open()
-        wx.TheClipboard.SetData(clipdata)
-        wx.TheClipboard.Close()
+        if wx.TheClipboard.Open():
+            try:
+                wx.TheClipboard.SetData(clipdata)
+            finally:
+                wx.TheClipboard.Close()
 
         dlg = wx.MessageDialog(
             self,
@@ -185,7 +215,7 @@ class MyFrame(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App(0)
-    frame_1 = MyFrame(None, -1, "OL TEST")
+    frame_1 = MyFrame(None, -1, "COMPTE INTERNET")
     app.SetTopWindow(frame_1)
     frame_1.Show()
     app.MainLoop()
