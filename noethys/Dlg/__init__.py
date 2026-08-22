c8b0bd861ae7ef7633a8641251b9a03354fcad28
fr4nck <f.bellardie@gmail.com>
2026-08-22 11:27:15 +0200

    UI: supprimer le routage de surcouche des préférences

--- noethys/Dlg/__init__.py
+++ noethys/Dlg/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """Chargements spécialisés de quelques dialogues lourds ou historiques.
 
-Le paquet reste paresseux pour les imports ordinaires. Les adaptateurs ci-dessous
-préservent les imports historiques tout en remplaçant uniquement le shell des
-fenêtres qui nécessitent une stabilisation Windows.
+Le paquet reste paresseux pour les imports ordinaires. Seuls les dialogues qui
+ont réellement besoin d'un chargement spécialisé sont routés ici. Les
+corrections de layout wxPython doivent rester dans leur module métier d'origine,
+pas dans un shell de substitution.
 """
 
 import importlib
@@ -11,7 +12,6 @@ import importlib
 
 _ADAPTATEURS = {
     "DLG_Impression_conso": ".DLG_Impression_conso_differe",
-    "DLG_Preferences": ".DLG_Preferences_stable",
 }
 
 
