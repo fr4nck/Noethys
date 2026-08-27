bfac75aa5b2f6149eb3dfbdb7d2f4c5d1eec2ff7
fr4nck <f.bellardie@gmail.com>
2026-08-27 15:25:08 +0200

    Noe-062A: tester types, cle primaire et preflight sans ecriture

--- tests/test_noe_062a_schema.py
+++ tests/test_noe_062a_schema.py
@@ -22,21 +22,49 @@ SCHEMA = _charger_module("noethys/Utils/UTILS_Tiers_Schema.py", "UTILS_Tiers_Sch
 DATA = _charger_module("noethys/Data/DATA_Structures.py", "DATA_Structures_noe062a_schema")
 
 
+def _schema_sqlite(nom_table):
+    resultat = []
+    for nom, type_champ, info in DATA.DB_STRUCTURES[nom_table]:
+        type_sql = type_champ
+        if type_sql == "LONGBLOB":
+            type_sql = "BLOB"
+        if type_sql == "BIGINT":
+            type_sql = "INTEGER"
+        type_prag = type_sql.split(" PRIMARY KEY", 1)[0]
+        pk = 1 if "PRIMARY KEY" in type_sql else 0
+        resultat.append((nom, type_prag, pk))
+    return resultat
+
+
 class FakeSchemaDB(object):
+    isNetwork = False
+
     def __init__(self, tables=None):
         self.tables = dict(tables or {})
         self.creations = []
         self.commits = 0
+        self._resultat = []
 
     def IsTableExists(self, nom_table):
         return nom_table in self.tables
 
-    def GetListeChamps2(self, nom_table):
-        return [(champ, "TEXT") for champ in self.tables[nom_table]]
+    def ExecuterReq(self, req):
+        if req.startswith("PRAGMA table_info('"):
+            nom_table = req.split("'", 2)[1]
+            colonnes = self.tables.get(nom_table, [])
+            self._resultat = [
+                (index, nom, type_sql, 0, None, pk)
+                for index, (nom, type_sql, pk) in enumerate(colonnes)
+            ]
+            return 1
+        return 0
+
+    def ResultatReq(self):
+        return list(self._resultat)
 
     def CreationTable(self, nom_table, dico):
         self.creations.append(nom_table)
-        self.tables[nom_table] = [champ[0] for champ in dico[nom_table]]
+        self.tables[nom_table] = _schema_sqlite(nom_table)
 
     def Commit(self):
         self.commits += 1
@@ -68,13 +96,13 @@ def test_activation_cree_uniquement_les_deux_tables_062a():
     assert db.creations == ["structures", "structures_contacts"]
     assert db.commits == 2
     assert set(db.tables) == {"structures", "structures_contacts"}
-    assert db.tables["structures"] == [champ[0] for champ in DATA.DB_STRUCTURES["structures"]]
+    assert [champ[0] for champ in db.tables["structures"]] == [champ[0] for champ in DATA.DB_STRUCTURES["structures"]]
 
 
 def test_activation_est_idempotente():
     tables = {
-        "structures": [champ[0] for champ in DATA.DB_STRUCTURES["structures"]],
-        "structures_contacts": [champ[0] for champ in DATA.DB_STRUCTURES["structures_contacts"]],
+        "structures": _schema_sqlite("structures"),
+        "structures_contacts": _schema_sqlite("structures_contacts"),
     }
     db = FakeSchemaDB(tables=tables)
     resultat = SCHEMA.AssurerSchema(db, appliquer=True)
@@ -86,14 +114,50 @@ def test_activation_est_idempotente():
 
 def test_table_existante_incomplete_n_est_jamais_modifiee_silencieusement():
     tables = {
-        "structures": ["IDstructure", "nom"],
-        "structures_contacts": [champ[0] for champ in DATA.DB_STRUCTURES["structures_contacts"]],
+        "structures": [("IDstructure", "INTEGER", 1), ("nom", "VARCHAR(300)", 0)],
+        "structures_contacts": _schema_sqlite("structures_contacts"),
     }
     db = FakeSchemaDB(tables=tables)
     resultat = SCHEMA.AssurerSchema(db, appliquer=True)
     assert resultat["ok"] is False
     assert resultat["tables_incoherentes"] == ("structures",)
     assert "uid" in resultat["rapport"]["structures"]["champs_manquants"]
+    assert db.creations == []
+    assert db.commits == 0
+
+
+def test_mauvais_type_de_cle_primaire_est_incompatible():
+    structures = _schema_sqlite("structures")
+    structures[0] = ("IDstructure", "TEXT", 1)
+    db = FakeSchemaDB({
+        "structures": structures,
+        "structures_contacts": _schema_sqlite("structures_contacts"),
+    })
+    resultat = SCHEMA.AssurerSchema(db, appliquer=False)
+    assert resultat["ok"] is False
+    assert "IDstructure" in resultat["rapport"]["structures"]["champs_incompatibles"]
+
+
+def test_absence_de_cle_primaire_est_incompatible():
+    structures = _schema_sqlite("structures")
+    structures[0] = ("IDstructure", "INTEGER", 0)
+    db = FakeSchemaDB({
+        "structures": structures,
+        "structures_contacts": _schema_sqlite("structures_contacts"),
+    })
+    resultat = SCHEMA.AssurerSchema(db, appliquer=False)
+    assert resultat["ok"] is False
+    assert "IDstructure" in resultat["rapport"]["structures"]["champs_incompatibles"]
+
+
+def test_preflight_incoherent_bloque_avant_creation_d_une_table_absente():
+    db = FakeSchemaDB({
+        "structures": [("IDstructure", "INTEGER", 1), ("nom", "VARCHAR(300)", 0)],
+    })
+    resultat = SCHEMA.AssurerSchema(db, appliquer=True)
+    assert resultat["ok"] is False
+    assert resultat["tables_incoherentes"] == ("structures",)
+    assert resultat["tables_absentes"] == ("structures_contacts",)
     assert db.creations == []
     assert db.commits == 0
 
