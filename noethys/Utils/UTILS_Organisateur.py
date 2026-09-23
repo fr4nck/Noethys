349132b70962780f58facfd4abc5babcfbe8f619
fr4nck <f.bellardie@gmail.com>
2026-09-23 19:34:14 +0200

    Vanilla: préserver la transparence du logo organisateur

--- noethys/Utils/UTILS_Organisateur.py
+++ noethys/Utils/UTILS_Organisateur.py
@@ -26,8 +26,9 @@ def GetNom():
         nom = ""
     return nom
 
-def RecadreImg(img=None, tailleImage=(40, 40)):
-    # Recadre l'image en fonction de la taille du staticBitmap
+def RecadreImg(img=None, tailleImage=(40, 40), fondBlanc=True):
+    # Recadre l'image en fonction de la taille du staticBitmap.
+    # Le ratio est conservé et les petits logos ne sont pas agrandis.
     tailleMaxi = max(tailleImage)
     largeur, hauteur = img.GetSize()
     if max(largeur, hauteur) > tailleMaxi :
@@ -42,10 +43,22 @@ def RecadreImg(img=None, tailleImage=(40, 40)):
         hauteur = int(hauteur)
     img.Rescale(width=largeur, height=hauteur, quality=wx.IMAGE_QUALITY_HIGH)
     position = (int((tailleImage[0] - largeur) / 2), int((tailleImage[1] - hauteur) / 2))
-    img.Resize(tailleImage, position, 255, 255, 255)
-    return img
 
-def GetDonnees(tailleLogo=(40, 40)) :
+    # Pour l'accueil, conserver le canal alpha évite de cuire un fond blanc
+    # dans le bitmap, source de halos sur un fond non blanc.
+    if fondBlanc or not img.HasAlpha():
+        img.Resize(tailleImage, position, 255, 255, 255)
+        return img
+
+    canvas = wx.Image(tailleImage[0], tailleImage[1])
+    canvas.InitAlpha()
+    canvas.SetRGB(wx.Rect(0, 0, tailleImage[0], tailleImage[1]), 255, 255, 255)
+    alpha = bytearray(tailleImage[0] * tailleImage[1])
+    canvas.SetAlpha(bytes(alpha))
+    canvas.Paste(img, position[0], position[1])
+    return canvas
+
+def GetDonnees(tailleLogo=(40, 40), fondLogoBlanc=True) :
     DB = GestionDB.DB()
     req = """SELECT nom, rue, cp, ville, tel, fax, mail, site, num_agrement, num_siret, code_ape, logo, logo_update
     FROM organisateur WHERE IDorganisateur=1;"""
@@ -74,7 +87,7 @@ def GetDonnees(tailleLogo=(40, 40)) :
             img = wx.Image(io, wx.BITMAP_TYPE_ANY)
         else :
             img = wx.ImageFromStream(io, wx.BITMAP_TYPE_ANY)
-        img = RecadreImg(img, tailleLogo)
+        img = RecadreImg(img, tailleLogo, fondBlanc=fondLogoBlanc)
         logo = img.ConvertToBitmap()
     dictDonnees = {
         "nom":nom, "rue":rue, "cp":cp, "ville":ville, "tel":tel, "fax":fax, "mail":mail, "site":site, 
