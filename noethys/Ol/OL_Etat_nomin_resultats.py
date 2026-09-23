d6ac8c1f9401c16fbae5d9e092eb07ca237ad943
fr4nck <f.bellardie@gmail.com>
2026-09-23 23:00:26 +0200

    Etats nominatifs: fiabiliser les catégories questionnaire

--- noethys/Ol/OL_Etat_nomin_resultats.py
+++ noethys/Ol/OL_Etat_nomin_resultats.py
@@ -287,8 +287,12 @@ class Track(object):
         for champ in listeChamps :
             if champ.type == "QUESTION" :
                 IDquestion = int(champ.code[len(champ.type):])
-                if champ.categorie == "Individu" : IDtemp = IDindividu
-                if champ.categorie == "Famille" : IDtemp = IDfamille
+                if champ.categorie == _(u"Individu") :
+                    IDtemp = IDindividu
+                elif champ.categorie == _(u"Famille") :
+                    IDtemp = IDfamille
+                else :
+                    continue
                 setattr(self, champ.code, GetReponse(IDquestion, IDtemp))
 
         # Regroupement des conso par UNITE puis PERIODE
